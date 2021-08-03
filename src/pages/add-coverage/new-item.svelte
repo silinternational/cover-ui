33c526a71470a510bc25778813b00f14e8c41993
Matt Henderson <matt_henderson@sil.org>
2021-08-03 14:05:16 -0400

    Remove "Item type" field

--- src/pages/add-coverage/new-item.svelte
+++ src/pages/add-coverage/new-item.svelte
@@ -1,31 +1,15 @@
 <script>
 import Description from '../../components/Description.svelte'
-import { Form, Page, Select, TextArea, TextField } from '@silintl/ui-components'
+import { Form, Page, TextArea, TextField } from '@silintl/ui-components'
 
-/* @todo Pull this from the database eventually: */
-const itemTypeOptions = [
-  {
-    "name": "Musical Instrument",
-    "id": "11111111-1111-4111-1111-111111111111",
-  },
-  {
-    "name": "Cell Phone",
-    "id": "22222222-2222-4222-2222-222222222222",
-  },
-]
 let shortName = ''
-let itemTypeUuid
 let itemDescription = ''
 
-const onChange = event => console.log('Item type changed:', event.detail)
 const onSubmit = event => console.log('Form submitted:', event.detail)
 </script>
 
 <Page>
   <Form on:submit={onSubmit}>
-    <p>
-      <Select label="Item type" bind:selectedID={itemTypeUuid} options={itemTypeOptions} on:change={onChange} />
-    </p>
     <p>
       <TextField label="Short name" bind:value={shortName}></TextField>
       <Description>This label will appear on your statements.</Description>

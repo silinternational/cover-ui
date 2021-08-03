4a35152bca326a6e89c2debab9fcecff99d02391
captaincrazybro <myeyesareonmizar@gmail.com>
2021-08-03 10:19:25 -0400

    Temporarily removed body width fix and made a redirect change to Breadcrumb

--- src/components/Breadcrumb.svelte
+++ src/components/Breadcrumb.svelte
@@ -16,7 +16,7 @@ function capitalize(string) {
 
 <!--TODO: change the '>' with the actual icon-->
 {#each urls as val, i}
-    <a href="https://{val.url}" style="margin: 0 3px;">{val.name}</a><!--
+    <a href="//{val.url}" style="margin: 0 3px;">{val.name}</a><!--
     -->{#if (urls.length - 1) != i}
         <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16" style="font-size: 1.5rem">
             <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>

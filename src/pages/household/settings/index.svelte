e45169f4877a0d48d64b60071852ba3d6c6b9777
captaincrazybro <myeyesareonmizar@gmail.com>
2021-08-03 08:43:53 -0400

    Fixed a few more bugs and made a custom, non-intereactive list

--- src/pages/household/settings/index.svelte
+++ src/pages/household/settings/index.svelte
@@ -6,46 +6,61 @@ import { prevent_default } from "svelte/internal";
     // TODO: make this dependent on backend
     let members = [
         {
-            name: "Levi Wenger",
+            name: "Alex Mercado",
             isYou: true,
             isDependent: false,
-            email: "levi_wenger@sil.org",
+            email: "alex_mercado@sil.org",
         },
         {
-            name: "Cameron Gordon",
+            name: "Megan Mercado",
             isYou: false,
-            isDependent: true,
+            isDependent: false,
+            email: "megan_mercado@sil.org",
+        },
+        {
+            name: "Pax Mercado",
+            isYou: false,
+            isDependent: true
         }
     ]
 </script>
 
 <style>
-    #members-div {
-        margin: 30px;
+    #members-page {
+        margin-top: 30px;
     }
 
-    .members-list:hover {
-        background-color: white;
+    #members-list {
+        counter-reset: item;
+        list-style-type: none;
+        padding-left: 0 !important;
+    }
+
+    .members-list-item {
+        border: 0 solid rgba(0, 0, 0, 0.12);
+        border-top-width: 1px !important;
+        padding: 10px;
     }
 </style>
 
 <Page>
     <Breadcrumb />
     <br />
-    <div id="members-div">
 
+    <div id="members-page">
+        <strong>Members</strong>
+
+        <ul id="members-list">
+            {#each members as m, i}
+                <li class="members-list-item">
+                    {m.name} {m.isYou ? "(you)" : ""}
+                    <br />
+                    <small>{m.isDependent ? "Dependent" : m.email}</small>
+                </li>
+                {#if i == (members.length - 1)}
+                    <li class="members-list-item"></li>
+                {/if}
+            {/each}
+        </ul>
     </div>
-    <strong>Members</strong>
-    <!--TODO: replace this with custom List component (doesn't have hover and select animation)-->
-    <List>
-        {#each members as m}
-            <List.Divider />
-            <List.Item>
-                {m.name} {m.isYou ? "(you)" : ""}
-                <br />
-                <small>{m.isDependent ? "Dependent" : m.email}</small>
-            </List.Item>
-        {/each}
-        <List.Divider />
-    </List>
 </Page>

3d78c0b9a918b42c9b04cf90ad5a11a20cb08707
hobbitronics <miguel.dw@gmail.com>
2021-08-02 23:28:54 -0400

    re-added template content for testing

--- src/pages/_layout.svelte
+++ src/pages/_layout.svelte
@@ -1,4 +1,6 @@
 <script>
+  import AppFooter from '../components/AppFooter.svelte'
+  import AppHeader from '../components/AppHeader.svelte'
   import { goto } from '@roxi/routify'
   import { Drawer } from '@silintl/ui-components'
 
@@ -34,10 +36,17 @@
   const logoClickHandler = () => $goto('/home')
 </script>
 
-<Drawer {menuItems} title='Riskman'>
+<Drawer modal {menuItems} title='Riskman'>
   <span class="pointer" on:click={logoClickHandler} slot="header">
     <img class="w-100" src="/logo.png" alt="logo">
   </span>
   
-  <slot />
+  <AppHeader />
+
+  <main>  
+    <slot />
+  </main>
+
+  <AppFooter />
+  
 </Drawer>

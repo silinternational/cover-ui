8ee6cd8b47013dfa69a618876fa09902131c77d2
Matt Henderson <matt_henderson@sil.org>
2021-08-03 15:26:20 -0400

    Fix indentation in style block

--- src/components/Description.svelte
+++ src/components/Description.svelte
@@ -1,8 +1,8 @@
 <style>
 .description {
-    color: rgba(0, 0, 0, 0.5);
-    display: block;
-    padding: 0.5rem 1rem;
+  color: rgba(0, 0, 0, 0.5);
+  display: block;
+  padding: 0.5rem 1rem;
 }
 </style>
 

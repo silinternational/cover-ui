04d3e7154c3e747b27ac07fe57bdc11ce65adbd3
captaincrazybro <myeyesareonmizar@gmail.com>
2021-08-03 11:36:11 -0400

    Added support for custom links for the breadcrumb

--- src/components/Breadcrumb.svelte
+++ src/components/Breadcrumb.svelte
@@ -1,24 +1,40 @@
 <script>
+import { goto, url } from "@roxi/routify";
+
+export let links
+
 let urls = []
-let href = window.location.href.replace("https://", "").replace("http://", "").split("/")
+if (!links) {
+  let href = $url().replace("https://", "").replace("http://", "").split("/")
 
-while (href.length > 1) {
-  let name = href[href.length - 1].toLowerCase()
-  urls.unshift({url: href.join("/"), name: capitalize(name) })
+  while (href.length > 1) {
+    let name = href[href.length - 1].toLowerCase()
+    urls.unshift({ url: href.join("/"), name: name })
 
-  href.pop()
+    href.pop()
+  }
+} else {
+  links.forEach((val, i) => {
+    let name = val.split("/")[val.split("/").length - 1]
+
+    urls.push({ url: val, name: name })
+  })
 }
 
-function capitalize(string) {
-  return string.replace(/^\w/, (c) => c.toUpperCase());
-}
 </script>
+
+<style>
+.breadcrumb-arrow {
+  margin: 0 3px;
+}
+</style>
 
 <!--TODO: change the '>' with the actual icon-->
 {#each urls as val, i}
-  <a href="//{val.url}" style="margin: 0 3px;">{val.name}</a><!--
+  <!-- svelte-ignore a11y-invalid-attribute -->
+  <a on:click={() => $goto(val.url)} href="" class="capitalize">{val.name}</a><!--
   -->{#if (urls.length - 1) != i}
-    <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16" style="font-size: 1.5rem">
+    <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="currentColor" class="bi bi-chevron-right breadcrumb-arrow" viewBox="0 0 16 16" style="font-size: 1.5rem">
       <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
     </svg>
   {/if}

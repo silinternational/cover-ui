4d8d63c6f0acd086d4ca8a33b3c6090560bebd19
Matt Henderson <matt_henderson@sil.org>
2021-08-03 14:26:58 -0400

    Add risk category selector

--- src/pages/add-coverage/new-item.svelte
+++ src/pages/add-coverage/new-item.svelte
@@ -1,11 +1,30 @@
 <script>
 import Description from '../../components/Description.svelte'
+import RadioOptions from '../../components/RadioOptions.svelte'
 import { Form, Page, TextArea, TextField } from '@silintl/ui-components'
 
 let shortName = ''
 let itemDescription = ''
+let riskCategoryOptions = [
+  {
+    'label': 'Carried with me',
+    'value': 'mobile',
+  },
+  {
+    'label': 'In one place (home or office)',
+    'value': 'stationary',
+  },
+]
+let riskCategory
 
-const onSubmit = event => console.log('Form submitted:', event.detail)
+const onSubmit = event => {
+  console.log('Form submitted:', event)
+  console.log(
+    shortName,
+    itemDescription,
+    riskCategory
+  )
+}
 </script>
 
 <Page>
@@ -17,6 +36,9 @@ const onSubmit = event => console.log('Form submitted:', event.detail)
     <p>
       <TextArea label="Item description" bind:value={itemDescription} rows="4"></TextArea>
       <Description>For personal use.</Description>
+    </p>
+    <p>
+      <RadioOptions name="riskCategory" options={riskCategoryOptions} bind:value={riskCategory} />
     </p>
   </Form>
 </Page>

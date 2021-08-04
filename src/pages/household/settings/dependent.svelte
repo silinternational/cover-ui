5487caa301f3e22b98b2c4ada93707b5ffd763ef
Matt Henderson <matt_henderson@sil.org>
2021-08-04 15:04:10 -0400

    Don't listen for "remove" events from "add new dependent" page

--- src/pages/household/settings/dependent.svelte
+++ src/pages/household/settings/dependent.svelte
@@ -6,10 +6,6 @@ import { Page } from '@silintl/ui-components'
 const onCancel = () => {
   $goto('../settings')
 }
-const onRemove = event => {
-  const dependentUuid = event.detail
-  console.log('Remove', dependentUuid)
-}
 const onSubmit = event => {
   const formData = event.detail
   console.log('DependentForm submitted', formData)
@@ -17,5 +13,5 @@ const onSubmit = event => {
 </script>
 
 <Page>
-  <DependentForm on:cancel={onCancel} on:remove={onRemove} on:submit={onSubmit} />
+  <DependentForm on:cancel={onCancel} on:submit={onSubmit} />
 </Page>

431c55a9644b9807e892ba80ee3bf2a87524bd9e
Matt Henderson <matt_henderson@sil.org>
2021-08-04 10:56:15 -0400

    Build some bottom-margin into the Breadcrumb component itself

--- src/components/Breadcrumb.svelte
+++ src/components/Breadcrumb.svelte
@@ -33,15 +33,21 @@ if (!links) {
 .breadcrumb-arrow {
   margin: 0 3px;
 }
+
+.bottom-margin {
+  margin-bottom: 1rem;
+}
 </style>
 
-<!--TODO: change the '>' with the actual icon-->
-{#each urls as val, i}
-  <!-- svelte-ignore a11y-invalid-attribute -->
-  <a on:click={() => $goto(val.url)} href="" class="capitalize">{val.name}</a><!--
-  -->{#if (urls.length - 1) != i}
-    <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="currentColor" class="bi bi-chevron-right breadcrumb-arrow" viewBox="0 0 16 16" style="font-size: 1.5rem">
-      <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
-    </svg>
-  {/if}
-{/each}
+<div class="bottom-margin">
+  <!--TODO: change the '>' with the actual icon-->
+  {#each urls as val, i}
+    <!-- svelte-ignore a11y-invalid-attribute -->
+    <a on:click={() => $goto(val.url)} href="" class="capitalize">{val.name}</a><!--
+    -->{#if (urls.length - 1) != i}
+      <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="currentColor" class="bi bi-chevron-right breadcrumb-arrow" viewBox="0 0 16 16" style="font-size: 1.5rem">
+        <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
+      </svg>
+    {/if}
+  {/each}
+</div>

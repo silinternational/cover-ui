8ec06038622f2cb8a86849190ae9f43d36b85402
Matt Henderson <matt_henderson@sil.org>
2021-08-04 13:11:25 -0400

    Add spouse/child option and remove button to DependentForm

--- src/components/DependentForm.svelte
+++ src/components/DependentForm.svelte
@@ -1,28 +1,87 @@
 <script>
+import RadioOptions from './RadioOptions.svelte'
 import { Button, Form, TextField } from '@silintl/ui-components'
 import { createEventDispatcher } from 'svelte'
 
 const dispatch = createEventDispatcher()
+const relationshipOptions = [
+  {
+    label: 'Spouse',
+    value: 'spouse',
+  },
+  {
+    label: 'Child',
+    value: 'child',
+  },
+]
+
+export let dependentUuid = undefined
 
 let name = ''
+let location = ''
+let relationship = ''
+let childBirthYear
 
-const onCancel = () => {
+$: console.log(relationship)
+
+const onCancel = event => {
+  event.preventDefault()
   dispatch('cancel')
 }
-const onSubmit = () => {
+const onRemove = event => {
+  event.preventDefault()
+  dispatch('remove', { name })
+}
+const onSubmit = event => {
+  event.preventDefault()
   dispatch('submit', { name })
 }
 </script>
 
+<style>
+.float-left {
+  float: left;
+}
+
+.float-right {
+  float: right;
+}
+
+.form-button {
+  margin: 0.5rem;
+}
+</style>
+
 <div class="w-50">
   <Form on:submit={onSubmit}>
-    <TextField label="Dependent Name" bind:value={name} autofocus />
+    <h4>Dependent</h4>
+    <p>
+      <TextField label="Dependent Name" bind:value={name} class="w-100" autofocus />
+    </p>
     <p>
       Dependents include non-member spouses and children under 26 who haven't
       married or finished college. Coverage for dependents is limited to $3,000
       per person.
     </p>
-    <Button on:click={onCancel}>Cancel</Button>
-    <Button raised>Save</Button>
+    <p>
+      <TextField label="Location" bind:value={location} class="w-100" />
+    </p>
+    <p>
+      <RadioOptions name="relationship" options={relationshipOptions} bind:value={relationship} />
+    </p>
+    {#if relationship === 'child'}
+      <p>
+        <TextField label="Child's birth year" bind:value={childBirthYear} class="w-100" />
+      </p>
+    {/if}
+    <div class="float-right form-button">
+      <Button raised>Save</Button>
+    </div>
+    <div class="float-right form-button">
+      <Button on:click={onCancel}>Cancel</Button>
+    </div>
+    <div class="float-left form-button">
+      <Button on:click={onRemove} outlined class="mdc-theme--error">Remove</Button>
+    </div>
   </Form>
 </div>

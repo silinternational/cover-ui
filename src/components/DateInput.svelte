2385ff4c8bcc8d5d3e7d2dd2b63aa47e77f9c525
captaincrazybro <myeyesareonmizar@gmail.com>
2021-08-04 14:13:33 -0400

    Added the focus function to DateInput

--- src/components/DateInput.svelte
+++ src/components/DateInput.svelte
@@ -1,8 +1,20 @@
 <script>
+import { MDCTextField } from '@material/textfield'
+import { onMount } from 'svelte'
 
 export let value = ''
 export let disabled = false
+export let autofocus = false
 
+let element = {}
+let mdcTextField = {}
+
+onMount(() => {
+  mdcTextField = new MDCTextField(element)
+  return () => mdcTextField.destroy()
+})
+
+const focus = node => autofocus && node.focus()
 </script>
 
 <style>
@@ -12,8 +24,9 @@ export let disabled = false
 </style>
 
 <label class="mdc-text-field mdc-text-field--outlined {$$props.class} textfield-radius"
-       class:mdc-text-field--disabled={disabled}>
-  <input type="date" class="mdc-text-field__input NotoSans date-input" bind:value on:blur {disabled}>
+       class:mdc-text-field--disabled={disabled}
+       bind:this={element}>
+  <input type="date" class="mdc-text-field__input NotoSans date-input" bind:value on:blur use:focus {disabled}>
   <span class="mdc-notched-outline">
     <span class="mdc-notched-outline__leading"></span>
     <span class="mdc-notched-outline__trailing"></span>

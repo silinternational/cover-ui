7373e4f5a9ed8cf87bff320c88b96e4dcb0df439
Michael Wilson <miguel.dw@gmail.com>
2021-08-04 14:50:58 -0400

    redesigned header with buttons

--- src/components/AppHeader.svelte
+++ src/components/AppHeader.svelte
@@ -1,55 +1,53 @@
 <script>
 import Error from './Error.svelte'
 import Progress from './progress/Progress.svelte'
-import { url, isActive } from '@roxi/routify'
+import { Badge, Button, IconButton, isAboveTablet } from '@silintl/ui-components'
+import { createEventDispatcher } from 'svelte';
 
-const rng = () => Math.floor(Math.random() * 1000) + 1
+let showImage = true
+let alt = 'avatar'
+let showDrawerButton
+
+const dispatch = createEventDispatcher()
+
+const user = {
+  nickname: 'Jon',
+  avatar_url: '',
+}
+
+$: src = user.avatar_url
+$: ownerInitial = user.nickname?.charAt(0) || ''
+
+const avatarError = () => showImage = false
+const openMenu = () => console.log('this will open the menu')
+const showOrHideDrawerToggle = () => isAboveTablet() ? (showDrawerButton = false) : (showDrawerButton = true)
+const toggleDrawerHandler = () => dispatch('toggleDrawer') //TODO toggle drawer
 </script>
 
 <style>
 header {
-  background-color: orange;
+  background-color: #fff;
   min-height: 4rem;
-  width: 100%;
-
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-}
-a {
-  padding-left: 1rem;
-  padding-right: 1rem;
-  padding-bottom: 0.25rem;
-  margin-left: 0.5rem;
-  margin-right: 0.5rem;
-
-  color: whitesmoke;
-  text-decoration: none;
-
-  font-size: x-large;
-}
-a:hover, .active {
-  background-color: whitesmoke;
-  color: black;
-
-  border-radius: 1rem;
 }
 </style>
+<svelte:window on:resize={showOrHideDrawerToggle}/>
 
-<header>
-  <a href={$url('/')} class:active={$isActive('/index')}>/</a>
-  <a href={$url('/login')} class:active={$isActive('/login')}>login</a>
-  <a href={$url('/home')} class:active={$isActive('/home')}>home</a>
-  <a href={$url('/non-existent')} class:active={$isActive('/non-existent')}>non-existent</a>
-  <a href={$url(`/param/${rng()}`)} class:active={$isActive('/param')}>param</a>
-  <a href={$url('/qs?a=1&b=2&c=3&d=4&d=5')} class:active={$isActive('/qs')}>qs</a>
-  <a href={$url('/env')} class:active={$isActive('/env')}>env</a>
-  <a href={$url('/sass')} class:active={$isActive('/sass')}>sass</a>
-  <a href={$url('/backend')} class:active={$isActive('/backend')}>backend</a>
-  <a href={$url('/errorhandling')} class:active={$isActive('/errorhandling')}>errorhandling</a>
-  <a href={$url('/progress')} class:active={$isActive('/progress')}>progress</a>
-  <a href={$url('/mdc')} class:active={$isActive('/mdc')}>mdc</a>
+<header class="flex justify-between align-items-center w-100">
+  <div class="flex justify-start">
+    {#if showDrawerButton}
+      <IconButton on:click={toggleDrawerHandler} icon={'menu'}/>
+    {/if}
+  </div>
+
+  <div class="flex justify-end">
+    <Button on:click={openMenu} class="pr-1">
+      {#if showImage && src}
+          <img {src} {alt} on:error={avatarError}/>
+      {:else}
+          <Badge padding='.4em' color='#005CB9'>{ownerInitial}</Badge>
+      {/if}
+    </Button>
+  </div>
 </header>
 
 <Progress />

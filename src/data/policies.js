ccfc9eee41a6781df2e3d1fbf297c52bf954c167
captaincrazybro <myeyesareonmizar@gmail.com>
2021-08-05 15:07:45 -0400

    actually added policies.js content 0.o

---
+++ src/data/policies.js
@@ -0,0 +1,94 @@
+import { writable } from 'svelte/store'
+import { GET, POST, PUT, DELETE } from './index'
+
+export const policies = writable([])
+export const loading = writable(false)
+export const isInitialized = writable(false)
+
+export function init() {
+    loadPolicies()
+}
+
+/**
+ *
+ * @description updates a current policy with the given 'policyData'
+ * @export
+ * @param {Number} id
+ * @param {Object} policyData
+ * @return {Object} 
+ */
+export async function updatePolicy(id, policyData) {
+    let parsedPolicyData = {
+        household_id: policyData.household_id,
+        cost_center: policyData.cost_center,
+        account: policyData.account,
+        entity_code: policyData.entity_code
+    }
+
+    let updatedPolicy = await PUT(`/policies/${id}`, parsedPolicyData)
+
+    policies.update(currPolicies => {
+        let i = currPolicies.findIndex(pol => pol.id == id)
+        currPolicies[i] = updatedPolicy
+        return currPolicies
+    })
+
+    return updatedPolicy
+}
+
+/**
+ *
+ * @description a function to fetch the items of a policy
+ * @export
+ * @param {Number} id
+ * @return {Object} 
+ */
+export async function getItems(id) {
+    loading.set(true)
+
+    let items = await GET(`/policies/${id}/items`)
+
+    loading.set(false)
+    return items
+}
+
+/**
+ *
+ * @description a function to create an item
+ * @export
+ * @param {Number} id
+ * @param {Object} itemData
+ * @return {Object} 
+ */
+export async function addItem(id, itemData) {
+    let parsedItemData = {
+        name: itemData.name,
+        category_id: itemData.category_id,
+        country: itemData.country,
+        description: itemData.description,
+        make: itemData.make,
+        model: itemData.model,
+        serial_number: itemData.serial_number,
+    }
+
+    let item = await POST(`/policies/${id}/items`, parsedItemData)
+
+    return item
+}
+
+export function clear() {
+    policies.set([])
+
+    isInitialized.set(false)
+}
+
+async function loadPolicies() {
+    loading.set(true)
+
+    const plcs = await GET('/policies')
+
+    policies.set(plcs)
+
+    loading.set(false)
+    isInitialized.set(true)
+}

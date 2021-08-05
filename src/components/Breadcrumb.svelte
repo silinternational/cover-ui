9c66d807c24c52d0875d591122a70f83eb010cbb
captaincrazybro <myeyesareonmizar@gmail.com>
2021-08-05 09:49:40 -0400

    Removed unes. comment

--- src/components/Breadcrumb.svelte
+++ src/components/Breadcrumb.svelte
@@ -51,9 +51,6 @@ a {
     <!-- svelte-ignore a11y-invalid-attribute -->
     <a on:click={() => $goto(val.url)} href="" class="capitalize">{val.name}</a><!--
     -->{#if (urls.length - 1) != i}
-      <!-- <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="currentColor" class="bi bi-chevron-right breadcrumb-arrow" viewBox="0 0 16 16" style="font-size: 1.5rem">
-        <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
-      </svg> -->
       <i class="material-icons mdc-list-item__graphic money-icon breadcrumb-arrow" aria-hidden="true">chevron_right</i>
     {/if}
   {/each}

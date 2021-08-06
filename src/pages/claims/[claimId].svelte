4ab8588cae8303efc4d5cbbcebad0aafc3619363
Matt Henderson <matt_henderson@sil.org>
2021-08-06 15:05:44 -0400

    Add a dummy claim-details page

---
+++ src/pages/claims/[claimId].svelte
@@ -0,0 +1,50 @@
+<script>
+import { Banner, MoneyInput, Row, SuccessBanner, WarningBanner } from '../../components'
+import { Button, Page } from '@silintl/ui-components'
+</script>
+
+<style>
+.left-detail {
+  margin: 0.5rem 0;
+}
+</style>
+
+<Page layout="grid">
+  <Row cols="3">
+    <h3 class="mdc-typography--headline5 my-0">Saxophone</h3>
+    <div class="left-detail">Claim 12345</div>
+    <Banner background="var(--mdc-theme-status-info-bg)"
+            color="var(--mdc-theme-status-info)">
+      <b>Theft</b>
+    </Banner>
+    <div class="left-detail">01 Jan 2001</div>
+  </Row>
+  <Row cols="9">
+    <SuccessBanner>
+      <b>Approved for replacement</b>
+    </SuccessBanner>
+    <WarningBanner>
+      <b>Needs changes: </b>
+      Upload a repair receipt to get reimbursed.
+    </WarningBanner>
+    <p>
+      Description sint blue bottle messenger bag. Vexillologist craft beer
+      normcore, taxidermy direct trade paleo echo park brooklyn stumptown lorem
+      biodiesel lomo meggings.
+    </p>
+    <p>
+      <b>Covered value</b><br />
+      $809.99
+    </p>
+    <p>
+      <b>Maximum payout</b><br />
+      $728.99
+    </p>
+    <p>
+      <Button outlined>Edit claim</Button>
+    </p>
+    <p>
+      <MoneyInput label="Actual cost of replacement"></MoneyInput>
+    </p>
+  </Row>
+</Page>

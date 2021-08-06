c622c84e1f31c7682188bde261a521e0e50e3532
Michael Wilson <miguel.dw@gmail.com>
2021-08-06 12:21:26 -0400

    created ClaimCards component

---
+++ src/components/ClaimCards.svelte
@@ -0,0 +1,21 @@
+<script>
+import { ClaimCard } from './index'
+
+export let exampleItems
+</script>
+
+<style>
+
+.cards {
+  margin: 8px 8px 8px 0;
+  flex-basis: 330px;
+}
+</style>
+
+<div class="flex justify-start flex-wrap {$$props.class}">
+  {#each exampleItems as item}
+    <div class="cards">
+      <ClaimCard {item} buttons={[ { label: "Edit coverage", url: "/items/edit-coverage" } ]} />
+    </div>
+  {/each}
+</div>

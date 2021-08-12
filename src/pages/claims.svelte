d5198f9667ec28d6c1f2ce9c4e53ca5b9921a808
Michael Wilson <miguel.dw@gmail.com>
2021-08-12 14:38:57 -0400

    control block if no claims

--- src/pages/claims.svelte
+++ src/pages/claims.svelte
@@ -3,44 +3,6 @@ import { claims } from '../data/claims.js'
 import { ClaimCards, Row } from '../components/'
 import { Page, Button } from '@silintl/ui-components'
 
-const exampleItems = [
-  {
-    name: "Saxophone",
-    accountable_person: "John Russel",
-    last_changed: "5 days",
-    state: { icon: 'chat' }
-  },
-  {
-    name: "GoPro",
-    accountable_person: "Priscilla Russel",
-    last_changed: "5 days",
-    state: { icon: 'chat' }
-  },
-  {
-    name: "GoPro",
-    accountable_person: "Priscilla Russel",
-    last_changed: "5 days",
-    state: { icon: 'chat' }
-  },
-  {
-    name: "GoPro",
-    accountable_person: "Priscilla Russel",
-    last_changed: "5 days",
-    state: { icon: 'chat' }
-  },
-  {
-    name: "GoPro",
-    accountable_person: "Priscilla Russel",
-    last_changed: "5 days",
-    state: { icon: 'chat' }
-  },
-  {
-    name: "GoPro",
-    accountable_person: "Priscilla Russel",
-    last_changed: "5 days",
-    state: { icon: 'chat' }
-  },
-]
 </script>
 
 <Page layout="grid">
@@ -49,6 +11,10 @@ const exampleItems = [
   </Row>
 
   <Row cols={'12'}>
-    <ClaimCards items={$claims} />
+    {#if $claims.length}
+      <ClaimCards items={$claims} />
+    {:else}
+      No claims at this time.
+    {/if}
   </Row>
 </Page>

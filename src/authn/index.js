fb84b0ad5b003aa333ad7763e72e57081e784a01
Matt Henderson <matt_henderson@sil.org>
2021-08-12 16:10:23 -0400

    Clean up imports in authn/index.js

--- src/authn/index.js
+++ src/authn/index.js
@@ -1,9 +1,8 @@
 
 import { clear as clearToken, getSeed, getToken } from './token'
 import { clear as clearUser } from './user'
-import { CREATE as POST, GET } from '../data'
+import { CREATE as POST } from '../data'
 import { throwError } from '../error'
-import { goto } from '@roxi/routify'
 
 export const login = async () => {
   const responseData = await POST(`auth/login/?client-id=${getSeed()}`)

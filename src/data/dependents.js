102ebc5a15646d4a10c02d4aa74ff0f1d160f6dd
Matt Henderson <matt_henderson@sil.org>
2021-08-17 13:32:50 -0400

    Adjust indentation/whitespace

--- src/data/dependents.js
+++ src/data/dependents.js
@@ -11,29 +11,29 @@ export const initialized = writable(false)
  * @export
  * @param {string} policyId -- The UUID for the desired policy
  * @param {Object} depData
- * @return {Object} 
+ * @return {Object}
  */
 export async function addDependent(policyId, depData) {
-    start(policyId)
-
-    let parsedDep = {
-      name: depData.name,
-      relationship: depData.relationship,
-      location: depData.location,
-      child_birth_year: depData.childBirthYear && parseInt(depData.childBirthYear)
-    }
-
-    let dpndt = await CREATE(`policies/${policyId}/dependents`, parsedDep)
-
-    dependents.update(currDeps => {
-      currDeps.push(parsedDep)
-      return currDeps
-    })
-
-    stop(policyId)
-
-    return parsedDep
-} 
+  start(policyId)
+  
+  let parsedDep = {
+    name: depData.name,
+    relationship: depData.relationship,
+    location: depData.location,
+    child_birth_year: depData.childBirthYear && parseInt(depData.childBirthYear)
+  }
+  
+  let dpndt = await CREATE(`policies/${policyId}/dependents`, parsedDep)
+  
+  dependents.update(currDeps => {
+    currDeps.push(parsedDep)
+    return currDeps
+  })
+  
+  stop(policyId)
+  
+  return parsedDep
+}
 
 /**
  *
@@ -42,28 +42,28 @@ export async function addDependent(policyId, depData) {
  * @param {string} dependentId -- The UUID for the desired dependent
  */
 export async function deleteDependent(dependentId) {
-    start(dependentId)
-
-    // TODO: uncomment when endpoint is finished
-    // await DELETE(`dependents/${depId}`)
-
-    dependents.update(currDeps => {
-      return currDeps.filter(dep => dep.id !== dependentId)
-    })
-
-    stop(dependentId)
+  start(dependentId)
+  
+  // TODO: uncomment when endpoint is finished
+  // await DELETE(`dependents/${depId}`)
+  
+  dependents.update(currDeps => {
+    return currDeps.filter(dep => dep.id !== dependentId)
+  })
+  
+  stop(dependentId)
 }
 
 /**
  *
- * @description a function to update a dependent 
+ * @description a function to update a dependent
  * @export
  * @param {string} dependentId -- The UUID for the desired dependent
  * @param {Object} depData
  */
 export async function updateDependent(dependentId, depData) {
   start(dependentId)
-
+  
   let parsedDep = {
     id: dependentId,
     name: depData.name,
@@ -71,16 +71,16 @@ export async function updateDependent(dependentId, depData) {
     location: depData.location,
     child_birth_year: depData.childBirthYear
   }
-
+  
   // TODO: uncomment when endpoint is finished
   // let dpndt = await UPDATE(`dependents/${dependentId}`)
-
+  
   dependents.update(currDeps => {
     let i = currDeps.findIndex(dep => dep.id === dependentId)
     currDeps[i] = parsedDep
     return currDeps
   })
-
+  
   stop(dependentId)
 }
 
@@ -92,11 +92,11 @@ export async function updateDependent(dependentId, depData) {
  */
 export async function loadDependents(policyId) {
   start(policyId)
-
+  
   let dpndts = await GET(`policies/${policyId}/dependents`)
-
+  
   dependents.set(dpndts)
-
+  
   stop(policyId)
   initialized.set(true)
 }

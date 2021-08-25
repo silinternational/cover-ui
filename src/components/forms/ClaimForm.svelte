7ca98b5100cf8b0c8a3a682675fee9a399db30f7
Matt Henderson <matt_henderson@sil.org>
2021-08-25 15:03:51 -0400

    Use new `formatMoney()` helper throughout UI code

--- src/components/forms/ClaimForm.svelte
+++ src/components/forms/ClaimForm.svelte
@@ -1,6 +1,7 @@
 <script>
 import { Description, RadioOptions, DateInput, MoneyInput } from '../../components'
 import { claimEventTypes, loadClaimEventTypes } from '../../data/claim-event-types'
+import { formatMoney } from '../../helpers/money'
 import { Button, Form, TextArea } from '@silintl/ui-components'
 import { createEventDispatcher } from 'svelte'
 
@@ -64,7 +65,7 @@ $: moneyPayoutOptions = [
   },
   // TODO: make this the min of either covered amount or FMV
   {
-    label: `Cash now ($${Number((fairMarketValue || 0) * regularFraction).toFixed(2)})`,
+    label: `Cash now (${formatMoney((fairMarketValue || 0) * regularFraction)})`,
     value: 'cash_now'
   }
 ]
@@ -164,7 +165,7 @@ const unSetRepairCost = () => {
       {/if}
     {/if}
     {#if repairableSelection === "repairable" && (isNotRepairableOrMoneyInputsAreSet && !seventyPercentCheck)}
-      <p>You will receive ${repairCost * regularFraction} to repair your insured Item.</p>
+      <p>You will receive {formatMoney(repairCost * regularFraction)} to repair your insured Item.</p>
     {/if}
     <!--TODO: add evacuation amount when items is done (covered_value*(2/3))-->
     <p>

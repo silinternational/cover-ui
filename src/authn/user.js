8f8e8cd69335808873bdfc853e90aa8757935f47
Matt Henderson <matt_henderson@sil.org>
2021-08-25 15:43:07 -0400

    Stop storing authenticated user's data in the browser's sessionStorage

--- src/authn/user.js
+++ src/authn/user.js
@@ -1,17 +1,15 @@
 import { writable } from 'svelte/store'
 import { GET } from '../data'
 
-const user = writable(JSON.parse(sessionStorage.getItem('authnUser')) || {})
+const user = writable({})
 
 export default user
 
 export async function loadUser() {
   const userData = await GET('users/me')
   user.set(userData)
-  sessionStorage.setItem('authnUser', JSON.stringify(userData))
 }
 
 export const clear = () => {
   user.set({})
-  sessionStorage.removeItem('authnUser')
 }

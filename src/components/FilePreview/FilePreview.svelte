b4d9f077e0b393d472f1e16fdd96387a3aa35843
hobbitronics <miguel.dw@gmail.com>
2021-09-08 17:38:32 -0400

    formatted date

--- src/components/FilePreview/FilePreview.svelte
+++ src/components/FilePreview/FilePreview.svelte
@@ -1,4 +1,5 @@
 <script>
+import { formatDate } from '../dates'
 import { Button, Progress } from '@silintl/ui-components'
 import { createEventDispatcher } from "svelte"
 import { flip } from 'svelte/animate'
@@ -25,10 +26,9 @@ function onDelete(event, id) {
 <div class="mt-10px py-10px">
   {#each previews as preview (preview.id)}
     <div transition:fly={{ y: 200, duration: 1500 }} animate:flip={{duration: 500}} class="preview flex justify-between align-items-center br-8px p-10px mb-1">
-      <!-- <img class="br-8px mr-10px" src={preview.src} alt={'receipt'} /> -->
       <div>
         <p class="white my-0">{preview.file.name}</p>
-        <p class="white my-0">{preview.created_at}</p>
+        <p class="white my-0">{formatDate(preview.created_at)}</p>
       </div>
       <Button class="delete-button" raised on:click={evt => onDelete(evt, preview.id)}>Delete</Button>
     </div>

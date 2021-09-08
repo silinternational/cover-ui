1ae3697d7a7432e79db09366a7788811f5b11d27
hobbitronics <miguel.dw@gmail.com>
2021-09-08 19:13:14 -0400

    highlight selected preview

--- src/components/FilePreview/FilePreview.svelte
+++ src/components/FilePreview/FilePreview.svelte
@@ -8,9 +8,28 @@ import { fly } from 'svelte/transition'
 export let previews = []
 export let uploading = false
 
+let selectedId = ''
+
 const dispatch = createEventDispatcher()
 
-const onClick = id => dispatch('preview', id)
+const isSelected = {}
+
+$: isSelected[selectedId] = true
+$: selectedId && setOthersFalse(selectedId)
+
+const setOthersFalse = id => {
+  for(const key of Object.keys(isSelected)){
+    if(key != id){
+      isSelected[key] = false
+    }
+  }
+}
+
+const onClick = id => {
+  selectedId = id
+
+  dispatch('preview', id)
+}
 
 function onDelete(event, id) {
   event.preventDefault()
@@ -24,11 +43,14 @@ function onDelete(event, id) {
 .preview {
   background-color: hsla(213, 26%, 23%, 1);
 }
+.selected {
+  background-color: hsla(213, 26%, 23%, .6);
+}
 </style>
 
 <div class="mt-10px py-10px">
   {#each previews as preview (preview.id)}
-    <div on:click|preventDefault={() => onClick(preview.id)} transition:fly={{ y: 200, duration: 1500 }} animate:flip={{duration: 500}} class="preview flex justify-between align-items-center br-8px p-10px mb-1">
+    <div on:click|preventDefault={() => onClick(preview.id)} transition:fly={{ y: 200, duration: 1500 }} animate:flip={{duration: 500}} class:selected={isSelected[preview.id]} class="preview flex justify-between align-items-center br-8px p-10px mb-1">
       <div>
         <p class="white my-0">{preview.file.name}</p>
         <p class="white my-0">{formatDate(preview.created_at)}</p>

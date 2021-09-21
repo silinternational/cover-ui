7d0b66f6fc0d5a16e76448b7fce0179cd87e783a
hobbitronics <miguel.dw@gmail.com>
2021-09-21 17:47:58 -0400

    moved determineMaxPayout to business-rules

--- src/business-rules/claim-payout-amount.ts
+++ src/business-rules/claim-payout-amount.ts
@@ -1,5 +1,7 @@
-import type { PayoutOption } from '../data/claims'
+import type { PayoutOption, ClaimItem } from '../data/claims'
+import { formatMoney } from '../helpers/money'
 
+export const deductible = .05
 export const LOSS_REASON_EVACUATION = 'Evacuation'
 
 export const PAYOUT_OPTION_FIXED_FRACTION: PayoutOption = 'FixedFraction'
@@ -43,4 +45,27 @@ export const isUnrepairableOrTooExpensive = (isRepairable, repairCostIsTooHigh) 
   }
   
   return repairCostIsTooHigh
+}
+
+const computePayout = (...values) => {
+  const filteredValues = [...values]?.filter(value => value !== undefined)
+  return formatMoney(Math.min(...filteredValues) * (1 - deductible))
+}
+
+const computeRepairMaxPayout = (claimItem: ClaimItem, coverageAmount) => computePayout(claimItem.repair_actual || claimItem.repair_estimate, coverageAmount, claimItem.fmv)
+
+const computeReplaceMaxPayout = (claimItem: ClaimItem, coverageAmount) => computePayout(claimItem.replace_estimate, coverageAmount)
+
+const computeCashMaxPayout = (claimItem: ClaimItem, coverageAmount) => computePayout(coverageAmount, claimItem.fmv)
+
+export const determineMaxPayout = (payoutOption, claimItem: ClaimItem, coverageAmount) => {
+  if(payoutOption === PAYOUT_OPTION_REPAIR) {
+    return computeRepairMaxPayout(claimItem, coverageAmount)
+  } else if(payoutOption === PAYOUT_OPTION_REPLACE) {
+    return computeReplaceMaxPayout(claimItem, coverageAmount)
+  } else if(payoutOption === PAYOUT_OPTION_FMV) {
+    return computeCashMaxPayout(claimItem, coverageAmount)
+  } else if(payoutOption === PAYOUT_OPTION_FIXED_FRACTION) {
+    return formatMoney(coverageAmount * 2/3) || ''
+  }
 }
--- src/pages/claims/[claimId].svelte
+++ src/pages/claims/[claimId].svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 import user from '../../authn/user'
+import { determineMaxPayout } from '../../business-rules/claim-payout-amount'
 import { Banner, Breadcrumb, ClaimBanner, ConvertCurrencyLink, FileDropArea, FilePreview, MoneyInput, Row } from '../../components'
 import { formatDate } from '../../components/dates'
 import { loading } from '../../components/progress'
@@ -17,8 +18,6 @@ const updatedClaimItemData = {} as any
 let showImg = false
 let repairOrReplacementCost: number
 let uploading = false
-let deductible = .05
-let maximumPayout: string
 let previewFile = {} as ClaimFile
 
 $: $initialized || loadClaims()
@@ -43,29 +42,13 @@ $: uploadLabel = getUploadLabel(claimItem, needsReceipt, receiptType) as string
 $: moneyFormLabel = needsRepairReceipt ? "Actual cost of repair" : "Actual cost of replacement"
 $: receiptType = needsRepairReceipt ? 'repair' : 'replacement'
 $: claimFiles = claim.claim_files || []
-$: if(payoutOption === 'Repair') {
-    maximumPayout = computeRepairMaxPayout()
-  } else if(payoutOption === 'Replacement') {
-    maximumPayout = computeReplaceMaxPayout()
-  } else if(payoutOption === 'FMV') {
-    maximumPayout = computeCashMaxPayout()
-  } else if(claim.incident_type === 'Evacuation') {
-    maximumPayout = formatMoney(item.coverage_amount * 2/3) || ''
-  }
+$: maximumPayout = determineMaxPayout(payoutOption, claimItem, item.coverage_amount) as string
 
 // Dynamic breadcrumbs data:
 $: claimName = `${item.name} (${claim.reference_number})`
 const claimsBreadcrumb = { name: 'Claims', url: '/claims' }
 $: thisClaimBreadcrumb = { name: claimName || 'This item', url: `/claims/${claimId}` }
 $: breadcrumbLinks = [claimsBreadcrumb, thisClaimBreadcrumb]
-
-const computePayout = (...values) => formatMoney(Math.min(...values) * (1 - deductible)) || ''
-
-const computeRepairMaxPayout = () => computePayout(claimItem.repair_estimate || claimItem.repair_actual, item.coverage_amount, claimItem.fmv)
-
-const computeReplaceMaxPayout = () => computePayout(claimItem.replace_estimate, item.coverage_amount)
-
-const computeCashMaxPayout = () => computePayout(item.coverage_amount, claimItem.fmv)
 
 const getFilePurpose = (claimItem: ClaimItem, needsReceipt: Boolean): ClaimFilePurpose => {
   if(needsReceipt) return 'Receipt'

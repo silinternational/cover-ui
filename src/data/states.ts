e5f0ef7cbb6ab239aa1ae36a1c21a7aaabfc0c7d
Michael Wilson <miguel.dw@gmail.com>
2021-09-22 16:24:54 -0400

    refactored states

--- src/data/states.ts
+++ src/data/states.ts
@@ -2,78 +2,89 @@ import type { ClaimStatus } from './claims'
 import type { ItemCoverageStatus } from './items'
 
 export type Status = ClaimStatus | ItemCoverageStatus
-export type Title = {
-  claim: string
-  item: string | null
-}
+
 export type State = {
   icon: string
   color: string
   bgColor: string
-  title: Title
+  title: string
+}
+export const approved = {
+  icon: 'paid',
+  color: '--mdc-theme-status-success',
+  bgColor: '--mdc-theme-status-success-bg',
+}
+
+export const pending: State = {
+  icon: 'watch_later',
+  color: '--mdc-theme-neutral-variant',
+  bgColor: '--mdc-theme-neutral-bg',
+  title: 'Awaiting',
 }
 
 export const warning: State = {
   icon: 'error',
   color: '--mdc-theme-status-warning',
   bgColor: '--mdc-theme-status-warning-bg',
-  title: { claim: 'Needs changes', item: 'Needs changes' },
+  title: 'Needs changes',
 }
-export const success: State = {
-  icon: 'done',
-  color: '--mdc-theme-status-success',
-  bgColor: '--mdc-theme-status-success-bg',
-  title: { claim: 'Approved for repair', item: 'Approved' },
-}
-export const pending: State = {
-  icon: 'watch_later',
-  color: '--mdc-theme-neutral-variant',
-  bgColor: '--mdc-theme-neutral-bg',
-  title: { claim: 'Awaiting review', item: 'Awaiting item coverage review' },
-}
-export const states: { [stateName: string]: State } = {
-  Revision: {
-    icon: 'chat',
-    color: '--mdc-theme-primary',
-    bgColor: '--mdc-theme-primary-header-bg',
-    title: { claim: '', item: null },
-  },
+
+export const commonStates: { [stateName: string]: State } = {
   Draft: {
     icon: 'edit',
     color: '--mdc-theme-primary',
     bgColor: '--mdc-theme-primary-header-bg',
-    title: { claim: 'Draft', item: 'Draft' },
+    title: 'Draft',
   },
-  Draft2: warning,
   Pending: pending,
-  Receipt: success,
-  Receipt2: warning,
-  Review1: pending,
-  Review2: pending,
-  Review3: pending,
   Denied: {
     icon: 'remove_circle',
     color: '--mdc-theme-status-error',
     bgColor: '--mdc-theme-status-error-bg',
-    title: { claim: 'Denied', item: 'Denied' },
+    title: 'Denied',
   },
-  Approved: {
-    icon: 'paid',
+}
+
+export const claimStates: { [stateName: string]: State } = {
+  ...commonStates,
+  Approved: { ...approved, title: 'Approved for payout' },
+  Draft2: warning,
+  Review1: pending,
+  Review2: pending,
+  Review3: pending,
+  Receipt2: warning,
+  Receipt: {
+    icon: 'done',
     color: '--mdc-theme-status-success',
     bgColor: '--mdc-theme-status-success-bg',
-    title: { claim: 'Approved for payout', item: 'Approved' },
+    title: 'Approved',
+  },
+  Revision: {
+    icon: 'chat',
+    color: '--mdc-theme-primary',
+    bgColor: '--mdc-theme-primary-header-bg',
+    title: '',
   },
   Paid: {
     icon: 'paid',
     color: '--mdc-theme-status-success',
     bgColor: '--mdc-theme-status-success-bg',
-    title: { claim: 'Complete', item: null },
+    title: 'Complete',
   },
 }
 
-export const getState = <State>(status: string) => {
+export const itemStates: { [stateName: string]: State } = {
+  ...commonStates,
+  Approved: { ...approved, title: 'Approved' },
+}
+
+export const getClaimState = <ClaimStatus>(status: string) => getAppropriateState(claimStates, status)
+
+export const getItemState = <ItemCoverageStatus>(status: string) => getAppropriateState(itemStates, status)
+
+const getAppropriateState = <State>(states: Status, status: Status) => {
   if (states[status] === undefined) {
-    console.error('No such state (for claim status):', status, Object.keys(states))
+    console.error('No such state (for claim/item status):', status, Object.keys(states))
   }
-  return states[status || 'Message']
+  return states[status] || ({} as State)
 }

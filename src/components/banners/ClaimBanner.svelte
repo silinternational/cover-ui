45f1566da1bdcb611385ec8fbad5bc62fa5ee507
Michael Wilson <miguel.dw@gmail.com>
2021-09-22 16:17:27 -0400

    imported FormattedBanner

--- src/components/banners/ClaimBanner.svelte
+++ src/components/banners/ClaimBanner.svelte
@@ -1,28 +1,11 @@
 <script lang="ts">
-import { Banner } from '../../components'
-import { getState, State } from '../../data/states'
+import { getClaimState, State } from '../../data/states'
+import type { ClaimStatus } from '../../data/claims'
+import { FormattedBanner } from '../index'
 
-export let claimStatus = ''
-export let isClaim: boolean = false
+export let claimStatus = '' as ClaimStatus
 
-$: state = (claimStatus && getState(claimStatus)) || ({} as State)
-
-$: bgColor = state.bgColor || ''
-$: color = state.color || ''
-$: icon = state.icon || ''
-$: title = isClaim ? state.title.claim : state.title.item || ''
+$: state = (claimStatus && getClaimState(claimStatus)) || ({} as State)
 </script>
 
-<style>
-i.material-icons {
-  margin-right: 0.5rem;
-}
-</style>
-
-<Banner background="var({bgColor})" color="var({color})" class={$$props.class}>
-  <i class="material-icons mdc-list-item__graphic" style="color: var({color});" aria-hidden="true">{icon}</i>
-  <div class="d-block">
-    <div class="mdc-typography--headline6" style="color: var({color});">{title}</div>
-    <div class="multi-line-truncate fs-14" style="color: var({color});"><slot /></div>
-  </div>
-</Banner>
+<FormattedBanner {state}><slot /></FormattedBanner>

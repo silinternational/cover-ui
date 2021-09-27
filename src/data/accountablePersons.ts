4a6bc3bb9cd0b5a2834a3c29c619258f0b4053d8
hobbitronics <miguel.dw@gmail.com>
2021-09-27 17:18:19 -0400

    added more functions accountablePersons

--- src/data/accountablePersons.ts
+++ src/data/accountablePersons.ts
@@ -1,6 +1,37 @@
 import type { PolicyItem } from './items'
+import { dependentsByPolicyId } from './dependents'
+import { membersByPolicyId } from './policy-members'
 
-export const getAccountablePerson = (item: PolicyItem, persons: any) => {
-  const id: string = item.accountable_user_id || item.accountable_dependent_id
+type AccountablePersons = {
+  id: string
+  name: string
+}
+
+export const getPolicyMemberOptions = (policyMembers: any[]): AccountablePersons[] =>
+  policyMembers.map((policyMember) => ({
+    id: policyMember.id,
+    name: policyMember.first_name + ' ' + policyMember.last_name,
+  }))
+
+export const getDependentOptions = (dependents: any[]): AccountablePersons[] =>
+  dependents.map((dependent) => ({
+    id: dependent.id,
+    name: dependent.name,
+  }))
+
+export const getAccountablePersonsByPolicyId = (id: string): AccountablePersons[] => {
+  let dependents: any
+  dependentsByPolicyId.subscribe((value) => (dependents = value[id]))
+  const dependentOptions = getDependentOptions(dependents)
+
+  let members: any
+  membersByPolicyId.subscribe((value) => (members = value[id]))
+  const policyMemberOptions = getPolicyMemberOptions(members)
+
+  return [...policyMemberOptions, ...dependentOptions]
+}
+
+export const getAccountablePerson = (item: PolicyItem, persons: any): AccountablePersons => {
+  const id: any = item.accountable_user_id || item.accountable_dependent_id
   return persons?.find((person: any) => person.id === id)?.name || ''
 }

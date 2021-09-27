7c33539d31633d4f749e7e28f3a77adedc5cbee2
Matt Henderson <matt_henderson@sil.org>
2021-09-27 17:35:21 -0400

    Add landing page for invitations

---
+++ src/pages/invite/[uuid].svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+import { loading } from '../../components/progress'
+import { CREATE } from '../../data'
+import { Page } from '@silintl/ui-components'
+import { onMount } from 'svelte'
+
+export let uuid: string
+
+onMount(async () => {
+  const response = await CREATE(`auth/login?invite=${encodeURIComponent(uuid)}`)
+
+  console.log(response) // TODO: Properly handle the API's response.
+})
+</script>
+
+<Page>
+  {#if $loading}
+    <p>Processing invitation...</p>
+  {/if}
+</Page>

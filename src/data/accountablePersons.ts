6d41e3ef9733f64d803d0462bdafd5453d05bc70
hobbitronics <miguel.dw@gmail.com>
2021-09-28 10:17:53 -0400

    decided not to async load in accountablePersons.ts

--- src/data/accountablePersons.ts
+++ src/data/accountablePersons.ts
@@ -1,28 +1,25 @@
 import type { PolicyItem } from './items'
-import { dependentsByPolicyId, loadDependents } from './dependents'
-import { loadMembersOfPolicy, membersByPolicyId } from './policy-members'
+import { dependentsByPolicyId } from './dependents'
+import { membersByPolicyId } from './policy-members'
 
-type AccountablePersons = {
+type AccountablePerson = {
   id: string
   name: string
 }
 
-export const getPolicyMemberOptions = (policyMembers: any[]): AccountablePersons[] =>
+export const getPolicyMemberOptions = (policyMembers: any[]): AccountablePerson[] =>
   policyMembers?.map((policyMember) => ({
     id: policyMember.id,
     name: policyMember.first_name + ' ' + policyMember.last_name,
   }))
 
-export const getDependentOptions = (dependents: any[]): AccountablePersons[] =>
+export const getDependentOptions = (dependents: any[]): AccountablePerson[] =>
   dependents?.map((dependent) => ({
     id: dependent.id,
     name: dependent.name,
   }))
 
-export const getAccountablePersonsByPolicyId = async (id: string): Promise<AccountablePersons[]> => {
-  await loadDependents(id)
-  await loadMembersOfPolicy(id)
-
+export const getAccountablePersonsByPolicyId = (id: string): AccountablePerson[] => {
   let dependents_value: any
   let members_value: any
 
@@ -38,7 +35,7 @@ export const getAccountablePersonsByPolicyId = async (id: string): Promise<Accou
   return [...policyMemberOptions, ...dependentOptions]
 }
 
-export const getAccountablePerson = (item: PolicyItem, persons: any): AccountablePersons => {
+export const getAccountablePerson = (item: PolicyItem, persons: any): AccountablePerson => {
   const id: any = item.accountable_user_id || item.accountable_dependent_id
   return persons?.find((person: any) => person.id === id)?.name || ''
 }

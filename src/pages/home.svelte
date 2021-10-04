ccdad4ae2badc8809508958d0a2660f85eeb5a0d
Matt Henderson <matt_henderson@sil.org>
2021-10-04 13:57:54 -0400

    Redirect stewards to their own home page

--- src/pages/home.svelte
+++ src/pages/home.svelte
@@ -1,6 +1,20 @@
 <script lang="ts">
+import user from '../authn/user'
 import { goto } from '@roxi/routify'
 
-// TODO: Eventually redirect stewards and signators to their own home pages.
-$goto('/customer/home')
+$: $user.app_role && sendToRoleHome($user.app_role)
+
+const sendToRoleHome = (appRole: string) => {
+  switch (appRole) {
+    case 'User':
+      $goto('/customer/home')
+      break
+    case 'Steward':
+      $goto('/steward/home')
+      break
+    default:
+      // TODO: Eventually redirect signators to their own home page, too.
+      console.error('Unknown role:', appRole)
+  }
+}
 </script>
---
+++ src/pages/steward/home.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+import { Row } from '../../components/'
+import { Page } from '@silintl/ui-components'
+</script>
+
+<style>
+</style>
+
+<Page layout="grid">
+  <Row cols={'12'}>(Claim cards that Steward can act on)</Row>
+
+  <Row cols={'12'}>(Recent activity)</Row>
+</Page>

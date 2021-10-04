3bef9d6a994287def9c491c798e66c1f1a060823
Matt Henderson <matt_henderson@sil.org>
2021-10-04 17:41:49 -0400

    Populate claim cards on steward home page

--- src/pages/steward/home.svelte
+++ src/pages/steward/home.svelte
@@ -1,13 +1,35 @@
 <script lang="ts">
-import { Row } from '../../components/'
+import { ClaimCards, Row } from '../../components/'
+import { Claim, claims, initialized as claimsInitialized, loadClaims, statusesAwaitingSteward } from '../../data/claims'
+import { itemsByPolicyId, loadItems } from '../../data/items'
 import { Page } from '@silintl/ui-components'
+
+let claimsAwaitingSteward: Claim[]
+
+$: $claimsInitialized || loadClaims()
+$: claimsAwaitingSteward = $claims.filter(isAwaitingSteward)
+
+$: claimsAwaitingSteward.map((claim) => claim.policy_id).forEach(loadOnce)
+
+$: items = [].concat(...Object.values($itemsByPolicyId))
+
+const isAwaitingSteward = (claim: Claim): boolean => {
+  return statusesAwaitingSteward.includes(claim.status)
+}
+const loadOnce = (policyId: string) => {
+  if (!Array.isArray($itemsByPolicyId[policyId])) {
+    loadItems(policyId)
+  }
+}
 </script>
 
 <style>
 </style>
 
 <Page layout="grid">
-  <Row cols={'12'}>(Claim cards that Steward can act on)</Row>
+  <Row cols={'12'}>
+    <ClaimCards claims={claimsAwaitingSteward} {items} />
+  </Row>
 
   <Row cols={'12'}>(Recent activity)</Row>
 </Page>

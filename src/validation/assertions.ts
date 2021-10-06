49b2dc43138d6940b1eac65c345392d7b4783f16
Michael Wilson <miguel.dw@gmail.com>
2021-10-06 16:23:01 -0400

    added typing to assertions

--- src/validation/assertions.ts
+++ src/validation/assertions.ts
@@ -1,12 +1,12 @@
 import { throwError } from '../error'
 
-export function assertHas(value, errorMessage) {
+export function assertHas(value: any, errorMessage: string): void {
   if (!value) {
     throwError(errorMessage)
   }
 }
 
-export function assertEmailAddress(email, errorMessage) {
+export function assertEmailAddress(email: string, errorMessage: string): void {
   const simpleEmailRegex = /\S+@\S+\.\S+/
 
   if (!simpleEmailRegex.test(email)) {

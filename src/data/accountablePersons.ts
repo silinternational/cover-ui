5e58e60248901f6a7684fd42359b0676c9213893
hobbitronics <miguel.dw@gmail.com>
2021-10-08 09:41:14 -0400

    accountable person shouldn't return an array

--- src/data/accountablePersons.ts
+++ src/data/accountablePersons.ts
@@ -24,5 +24,5 @@ export const getDependentOptions = (dependents: any[]): AccountablePersonOptions
 
 export const getAccountablePerson = (item: PolicyItem, persons: any): AccountablePersonOptions => {
   const id: any = item.accountable_user_id || item.accountable_dependent_id
-  return persons?.find((person: any) => person.id === id) || []
+  return persons?.find((person: any) => person.id === id) || {}
 }

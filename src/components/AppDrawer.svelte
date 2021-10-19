d0876192852c4083414f0c10c172b6c5a0039ad8
Matt Henderson <matt_henderson@sil.org>
2021-10-19 15:17:57 -0400

    Get a minimal version of the role-menu working locally

--- src/components/AppDrawer.svelte
+++ src/components/AppDrawer.svelte
@@ -2,15 +2,35 @@
 import AppFooter from './AppFooter.svelte'
 import AppHeader from './AppHeader.svelte'
 import { goto } from '@roxi/routify'
-import { Drawer } from '@silintl/ui-components'
+import { Button, Drawer, Menu } from '@silintl/ui-components'
 import { ROOT } from 'helpers/routes'
 
 export let menuItems: any[] = []
 
+const roleMenuItems = [
+  { icon: 'gavel', label: 'Steward', url: '/steward/home' },
+  { icon: 'work', label: 'Department Name', url: '/customer/home/[departmentPolicyId]' },
+  { icon: 'family_restroom', label: 'Household', url: '/customer/home/[householdPolicyId]' },
+]
+
+let roleMenuOpen = false
+let roleMenuSelection = 'Steward' // TEMP example
 let toggle = false
 
 const logoClickHandler = () => $goto(ROOT)
+const toggleRoleMenu = () => {
+  roleMenuOpen = !roleMenuOpen
+}
 </script>
+
+<style>
+.role-menu {
+  margin-left: 12px;
+}
+#role-menu-options-container {
+  position: absolute;
+}
+</style>
 
 <Drawer miniMenu modal hideForPhonesOnly {toggle} {menuItems} title="Covered" class="auto-width border-white">
   <span class="pointer" on:click={logoClickHandler} slot="header">
@@ -18,6 +38,13 @@ const logoClickHandler = () => $goto(ROOT)
   </span>
 
   <AppHeader on:toggleDrawer={() => (toggle = !toggle)} />
+
+  <div class="role-menu pt-1" slot="drawer-content-top">
+    <Button appendIcon="arrow_drop_down" on:click={toggleRoleMenu}>{roleMenuSelection}</Button>
+    <div id="role-menu-options-container">
+      <Menu bind:menuOpen={roleMenuOpen} menuItems={roleMenuItems} />
+    </div>
+  </div>
 
   <slot />
 

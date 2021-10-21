bd9d645f270d0e10475e05618ec601f464646302
Michael Wilson <miguel.dw@gmail.com>
2021-10-21 12:17:31 -0400

    gave itemId a different state when unpaid

--- src/pages/items/[itemId].svelte
+++ src/pages/items/[itemId].svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 import user from '../../authn/user'
 import { Banner, Breadcrumb, ItemBanner, ItemDeleteModal, Row } from 'components'
-import { formatDate } from 'components/dates'
+import { getYear, formatDate } from 'components/dates'
 import { loading } from 'components/progress'
 import {
   AccountablePersonOptions,
@@ -11,10 +11,11 @@ import {
 } from 'data/accountablePersons'
 import { dependentsByPolicyId, loadDependents } from 'data/dependents'
 import { deleteItem, ItemCoverageStatus, itemsByPolicyId, loadItems, PolicyItem } from 'data/items'
+import { paymentReceivedByYearByPolicyId } from 'data/payment'
 import { init, policies, Policy } from 'data/policies'
 import { loadMembersOfPolicy, membersByPolicyId } from 'data/policy-members'
 import { formatMoney } from 'helpers/money'
-import { ITEMS, item as itemRoute, itemEdit, itemNewClaim } from 'helpers/routes'
+import { ITEMS, item as itemRoute, itemEdit, itemNewClaim, CUSTOMER_HOME } from 'helpers/routes'
 import { formatPageTitle } from 'helpers/pageTitle'
 import { goto, metatags } from '@roxi/routify'
 import { Button, Page, Dialog } from '@silintl/ui-components'
@@ -55,9 +56,14 @@ $: item = items.find((itm) => itm.id === itemId) || ({} as PolicyItem)
 $: itemName = item.name || ''
 $: status = (item.coverage_status || '') as ItemCoverageStatus
 $: status === 'Draft' && $user.app_role === 'User' && goToEditItem()
+$: hasPaid = $paymentReceivedByYearByPolicyId[policyId]?.[year]
+$: unpaid = !hasPaid
+$: org = policy?.entity_code?.name
 
 $: submittedText = item.updated_at ? formatDistanceToNow(Date.parse(item.updated_at), { addSuffix: true }) : ''
 $: startDate = formatDate(item.coverage_start_date)
+$: year = getYear(startDate)
+$: renewYear = Number(year) + 1
 
 $: allowRemoveCovereage = !['Inactive', 'Denied'].includes(status) as boolean
 
@@ -82,9 +88,28 @@ const handleDialog = async (event: CustomEvent<string>) => {
     $goto(ITEMS)
   }
 }
+
+const onPayment = () => {
+  $paymentReceivedByYearByPolicyId[policyId] = { [year]: true }
+
+  console.log('sending payment') //TODO call endpoint when ready
+}
 </script>
 
-<Page layout={'grid'}>
+<style>
+.payment-header {
+  background-color: var(--mdc-theme-neutral-bg);
+  padding: 4px;
+  border-radius: 8px 8px 0 0;
+}
+
+.unpaid {
+  background-color: var(--mdc-theme-neutral-9);
+  border-radius: 0 0 8px 8px;
+}
+</style>
+
+<Page>
   {#if !item.id}
     {#if $loading}
       Loading...
@@ -92,60 +117,83 @@ const handleDialog = async (event: CustomEvent<string>) => {
       We could not find that item. Please <a href={ITEMS}>go back</a> and select an item from the list.
     {/if}
   {:else}
-    <Row cols="12">
+    {#if hasPaid}
       <div class="flex justify-between align-items-center">
         <Breadcrumb links={breadcrumbLinks} />
         <div>
           {#if allowRemoveCovereage}
             <Button class="remove-button mx-5px" on:click={() => (open = true)}>Remove</Button>
           {/if}
-          <ItemDeleteModal {open} {item} on:closed={handleDialog} />
           {#if status === 'Draft' || status === 'Pending'}
             <Button on:click={goToEditItem}>Edit Item</Button>
           {/if}
         </div>
       </div>
-    </Row>
-
-    <Row cols="3">
-      <h2 class="break-word my-1">{item.name || ''}</h2>
-      <b>Covered value</b>
-      <div class="my-2px">{formatMoney(item.coverage_amount)}</div>
-      <b>Annual premium</b>
-      <div class="my-2px">{formatMoney(item.annual_premium)}</div>
-      <br />
-      <b>{accountablePersonName || ''}</b>
-      <div class="mt-4px">Household ID</div>
-      <div>{householdId}</div>
-    </Row>
-
-    <Row cols="9">
-      <ItemBanner itemStatus={status}>Submitted {submittedText}</ItemBanner>
-      <h3 class="break-word">{item.make || ''} {item.model || ''}</h3>
-      {#if item.serial_number}
-        <b class="mb-6px">Unique ID</b>
-        <div class="break-word">{item.serial_number}</div>
+    {:else}
+      <h2>Review Coverage and Checkout</h2>
+      <div class="payment-header flex justify-between align-items-center">
+        <Button class="mx-5px" on:click={() => (open = true)}>Discard</Button>
+        <div>
+          <Button outlined on:click={$goto(CUSTOMER_HOME)}>Save for later</Button>
+          <Button raised on:click={goToEditItem}>Edit Item</Button>
+        </div>
+      </div>
+    {/if}
+    <ItemDeleteModal {open} {item} on:closed={handleDialog} />
+    <div class="flex p-1" class:unpaid>
+      <div class="w-25">
+        <h2 class="break-word my-1">{item.name || ''}</h2>
+        <b>Covered value</b>
+        <div class="my-2px">{formatMoney(item.coverage_amount)}</div>
+        <b>Annual premium</b>
+        <div class="my-2px">{formatMoney(item.annual_premium)}</div>
         <br />
-      {/if}
-      <div class="break-word">Description: {item.description || ''}</div>
-      <br />
-      <Banner
-        background="var(--mdc-theme-primary-header-bg)"
-        color="var(--mdc-theme-primary)"
-        class="max-content-width"
-      >
-        {item.category?.name || ''}
-      </Banner>
-      <div class="my-1">
-        <b>Starts</b>
-        <div>{startDate}</div>
+        <b>{accountablePersonName || ''}</b>
+        <div class="mt-4px">Household ID</div>
+        <div>{householdId}</div>
       </div>
-      <br />
-      <div>
-        {#if status === 'Approved'}
-          <Button class="mdc-theme--secondary-background" on:click={goToNewClaim} raised>File Claim</Button>
+
+      <div class="w-75">
+        {#if hasPaid}
+          <ItemBanner itemStatus={status}>Submitted {submittedText}</ItemBanner>
         {/if}
+        <h3 class="break-word">{item.make || ''} {item.model || ''}</h3>
+        {#if item.serial_number}
+          <b class="mb-6px">Unique ID</b>
+          <div class="break-word">{item.serial_number}</div>
+          <br />
+        {/if}
+        <div class="break-word">Description: {item.description || ''}</div>
+        <br />
+        <Banner
+          background="var(--mdc-theme-primary-header-bg)"
+          color="var(--mdc-theme-primary)"
+          class="max-content-width"
+        >
+          {item.category?.name || ''}
+        </Banner>
+        <div class="my-1">
+          <b>Starts</b>
+          <div>{startDate}</div>
+        </div>
+        <br />
+        <div>
+          {#if hasPaid}
+            {#if status === 'Approved'}
+              <Button class="mdc-theme--secondary-background" on:click={goToNewClaim} raised>File Claim</Button>
+            {/if}
+          {/if}
+        </div>
       </div>
-    </Row>
+    </div>
+    {#if !hasPaid}
+      <div class="flex p-1 mt-2">
+        <div>
+          Pay {formatMoney(item.annual_premium)} for the remainder of {year} from {org} account {householdId}.
+          Auto-renew on 1 January {renewYear}.
+        </div>
+        <Button class="ml-1" raised on:click={onPayment}>Agree and Pay {formatMoney(item.annual_premium)}</Button>
+      </div>
+    {/if}
   {/if}
 </Page>

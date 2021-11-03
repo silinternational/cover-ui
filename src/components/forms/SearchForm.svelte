bfc99b544029ae7206b5f3c3c11467123d82ce32
Matt Henderson <matt_henderson@sil.org>
2021-11-03 13:38:30 -0400

    Extract SearchForm to reduce complexity in "/admin/policies" page

---
+++ src/components/forms/SearchForm.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+import { createEventDispatcher } from 'svelte'
+import { Form, TextField } from '@silintl/ui-components'
+
+const dispatch = createEventDispatcher()
+
+let searchFieldContents = ''
+
+const onSubmit = () => dispatch('search', searchFieldContents)
+</script>
+
+<Form on:submit={onSubmit}>
+  Search:
+  <TextField bind:value={searchFieldContents} />
+</Form>

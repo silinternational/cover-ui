5fc9b74b14eda59b74315aed2c0ee492f69e1c86
Matt Henderson <matt_henderson@sil.org>
2021-11-08 13:57:11 -0500

    If a policy is already selected, have "/customer/home" go there

--- src/pages/customer/home.svelte
+++ src/pages/customer/home.svelte
@@ -1,17 +1,22 @@
 <script lang="ts">
 import user, { User } from '../../authn/user'
 import { Policy } from 'data/policies'
+import { selectedPolicyId } from 'data/role-policy-selection'
 import { policyHome } from 'helpers/routes'
 import { redirect } from '@roxi/routify'
 
-$: $user.id && redirectToAppropriatePolicy($user)
+$: redirectToAppropriatePolicy($user, $selectedPolicyId)
 
-const redirectToAppropriatePolicy = (user: User) => {
-  const policies = user.policies || []
-  const corporatePolicies = policies.filter((p: Policy) => p.type === 'Corporate')
-  const householdPolicies = policies.filter((p: Policy) => p.type === 'Household')
+const redirectToAppropriatePolicy = (user: User, selectedPolicyId: string) => {
+  if (selectedPolicyId) {
+    $redirect(policyHome(selectedPolicyId))
+  } else if (user.id) {
+    const policies = user.policies || []
+    const corporatePolicies = policies.filter((p: Policy) => p.type === 'Corporate')
+    const householdPolicies = policies.filter((p: Policy) => p.type === 'Household')
 
-  const policyIdToUse = corporatePolicies[0]?.id || householdPolicies[0]?.id || user.policy_id
-  $redirect(policyHome(policyIdToUse))
+    const policyIdToUse = corporatePolicies[0]?.id || householdPolicies[0]?.id || user.policy_id
+    $redirect(policyHome(policyIdToUse))
+  }
 }
 </script>

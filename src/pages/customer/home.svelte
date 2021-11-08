581453b53b8497859f5ec0d68edab6afd4cdbf5f
Matt Henderson <matt_henderson@sil.org>
2021-11-08 13:47:26 -0500

    Default customer to corporate policy if they have one, household if not

--- src/pages/customer/home.svelte
+++ src/pages/customer/home.svelte
@@ -1,9 +1,17 @@
 <script lang="ts">
-import user from '../../authn/user'
+import user, { User } from '../../authn/user'
+import { Policy } from 'data/policies'
 import { policyHome } from 'helpers/routes'
 import { redirect } from '@roxi/routify'
 
-/* TODO: Load customer's corporate policies (if any) and intelligently decide
- * which policy to default to. */
-$: $user.policy_id && $redirect(policyHome($user.policy_id))
+$: $user.id && redirectToAppropriatePolicy($user)
+
+const redirectToAppropriatePolicy = (user: User) => {
+  const policies = user.policies || []
+  const corporatePolicies = policies.filter((p: Policy) => p.type === 'Corporate')
+  const householdPolicies = policies.filter((p: Policy) => p.type === 'Household')
+
+  const policyIdToUse = corporatePolicies[0]?.id || householdPolicies[0]?.id || user.policy_id
+  $redirect(policyHome(policyIdToUse))
+}
 </script>

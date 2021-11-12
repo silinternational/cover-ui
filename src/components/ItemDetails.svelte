4cc38ec64cfb6aa95f3125d5827f8face2e8aa7e
Russell Champoux <RussellChamp@gmail.com>
2021-11-12 11:10:14 -0500

    update item details with country details

--- src/components/ItemDetails.svelte
+++ src/components/ItemDetails.svelte
@@ -19,12 +19,24 @@ let policy: Policy
 onMount(() => loadPolicy(policyId))
 
 $: $policies && (policy = getPolicyById(policyId))
-$: householdId = policy.household_id ? policy.household_id : ''
 
 $: statusText = getItemStatusText(item)
 $: status = (item.coverage_status || '') as ItemCoverageStatus
 $: showRevisionMessage = item.status_reason && status === 'Revision'
 $: startDate = formatDate(item.coverage_start_date)
+$: endDate = formatDate(item.coverage_end_date)
+$: sidebarItems = {
+  'Accountable Person': item?.accountable_person?.name,
+  Location: item.country,
+  'Household ID': policy?.household_id,
+  'Covered value': formatMoney(item.coverage_amount),
+  Premium: `${formatMoney(item.annual_premium)} / yr`,
+}
+$: bodyItems = {
+  Model: `${item?.make} ${item?.model}`,
+  'Unique ID': item?.serial_number,
+  Description: item?.description,
+}
 
 const getItemStatusText = (item: PolicyItem) => {
   const updatedAtStr = item.updated_at ? formatDistanceToNow(Date.parse(item.updated_at), { addSuffix: true }) : ''
@@ -39,42 +51,74 @@ const getItemStatusText = (item: PolicyItem) => {
   background-color: var(--mdc-theme-neutral-9);
   border-radius: 0 0 8px 8px;
 }
+.sidebar-item,
+.body-item {
+  margin-bottom: 1em;
+}
+.title {
+  margin-bottom: 0.4em;
+}
+.value {
+  font-size: x-large;
+}
+.coverage-dates {
+  margin-top: 1em;
+  display: flex;
+}
+.start-date div {
+  margin-right: 2em;
+}
+.banners {
+  margin-bottom: 0.5em;
+}
 </style>
 
 <div class="flex p-1" class:isCheckingOut>
-  <div class="w-25">
+  <div class="w-25 sidebar">
     <h2 class="break-word my-1">{item.name || ''}</h2>
-    <b>Covered value</b>
-    <div class="my-2px">{formatMoney(item.coverage_amount)}</div>
-    <b>Annual premium</b>
-    <div class="my-2px">{formatMoney(item.annual_premium)}</div>
-    <br />
-    <b>{item.accountable_person?.name || ''}</b>
-    <div class="mt-4px">Household ID</div>
-    <div>{householdId}</div>
+
+    {#each Object.entries(sidebarItems) as [title, value]}
+      {#if title && value}
+        <div class="sidebar-item">
+          <div class="title"><b>{title}</b></div>
+          <div class="value">{value}</div>
+        </div>
+      {/if}
+    {/each}
   </div>
 
   <div class="w-75">
     {#if !isCheckingOut}
-      <ItemBanner itemStatus={status} {isAdmin}>{statusText}</ItemBanner>
-      {#if showRevisionMessage}
-        <MessageBanner class="mt-4px">{item.status_reason}</MessageBanner>
+      <div class="banners">
+        <ItemBanner itemStatus={status} {isAdmin}>{statusText}</ItemBanner>
+        {#if showRevisionMessage}
+          <MessageBanner class="mt-4px">{item.status_reason}</MessageBanner>
+        {/if}
+      </div>
+    {/if}
+
+    {#each Object.entries(bodyItems) as [title, value]}
+      {#if title && value}
+        <div class="body-item">
+          <div class="title"><b>{title}</b></div>
+          <div class="value break-word">{value}</div>
+        </div>
       {/if}
-    {/if}
-    <h3 class="break-word">{item.make || ''} {item.model || ''}</h3>
-    {#if item.serial_number}
-      <b class="mb-6px">Unique ID</b>
-      <div class="break-word">{item.serial_number}</div>
-      <br />
-    {/if}
-    <div class="break-word">Description: {item.description || ''}</div>
-    <br />
+    {/each}
+
     <Banner background="var(--mdc-theme-primary-header-bg)" color="var(--mdc-theme-primary)" class="max-content-width">
       {item.category?.name || ''}
     </Banner>
-    <div class="my-1">
-      <b>Starts</b>
-      <div>{startDate}</div>
+
+    <div class="coverage-dates">
+      <div class="start-date">
+        <b>Coverage starts</b>
+        <div class="value">{startDate}</div>
+      </div>
+      <div class="end-date">
+        <b>Coverage ends</b>
+        <div class="value">{endDate || '—'}</div>
+      </div>
     </div>
   </div>
 </div>

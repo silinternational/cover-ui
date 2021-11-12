de39583867f83b5b82c4ed25bf063ca299adae80
Michael Wilson <miguel.dw@gmail.com>
2021-11-12 11:57:53 -0500

    filter inactive items from recent cards

--- src/components/CardsGrid.svelte
+++ src/components/CardsGrid.svelte
@@ -37,7 +37,7 @@ const parseRecentChanges = (changes: RecentChange[]): CardItem[] => {
   for (let change of changes) {
     if (isRecentClaim(change)) {
       cards.push({ data: change.Claim?.claim_items[0], claim: change.Claim })
-    } else {
+    } else if (change.Item?.coverage_status !== 'Inactive') {
       cards.push({ data: change.Item })
     }
   }

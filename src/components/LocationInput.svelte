25f78ad7351b2c090624392199b2d0c2f350209f
Matt Henderson <matt_henderson@sil.org>
2021-11-15 16:57:58 -0500

    Begin making new LocationInput component

---
+++ src/components/LocationInput.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+import GooglePlacesAutocomplete from '@silintl/svelte-google-places-autocomplete'
+
+export let value = ''
+
+const googlePlacesApiKey = process.env.GOOGLE_PLACES_API_KEY
+const options = {
+  fields: ['address_components', 'geometry'],
+  types: ['country'],
+}
+const placeholder = 'Enter country'
+</script>
+
+<label class="mdc-text-field mdc-text-field--outlined textfield-radius mdc-text-field--no-label">
+  <GooglePlacesAutocomplete
+    apiKey={googlePlacesApiKey}
+    class="mdc-text-field__input"
+    {options}
+    on:place_changed
+    {placeholder}
+    on:ready
+    {value}
+  />
+  <span class="mdc-notched-outline mdc-notched-outline--no-label">
+    <span class="mdc-notched-outline__leading" /> <span class="mdc-notched-outline__trailing" />
+  </span>
+</label>

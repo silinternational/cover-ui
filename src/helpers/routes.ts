b2431e226659aee2b01d2dc80a59936dfabd8f09
Michael Wilson <miguel.dw@gmail.com>
2021-11-17 13:21:05 -0500

    rename pathIsNotPolicySettings

--- src/helpers/routes.ts
+++ src/helpers/routes.ts
@@ -46,4 +46,4 @@ export const householdSettingsNewDependent = (policyId: string) => `/policies/${
 export const TERMS_OF_SERVICE = '/terms'
 export const PRIVACY_POLICY = '/privacy'
 
-export const routeIsNotPolicySettings = (path: string): boolean => path !== '/policies/:policyId/settings'
+export const pathIsNotPolicySettings = (path: string): boolean => path !== '/policies/:policyId/settings'

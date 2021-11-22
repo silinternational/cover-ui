510a73776cd66b158e33dfee81d155ed880be3df
hobbitronics <miguel.dw@gmail.com>
2021-11-22 10:15:29 -0700

    added CountrySelector

---
+++ src/components/CountrySelector.svelte
@@ -0,0 +1,30 @@
+<script type="ts">
+import SearchableSelect from './SearchableSelect.svelte'
+import { countries, Country } from 'data/countries'
+import { setNotice } from '@silintl/ui-components'
+import { createEventDispatcher } from 'svelte'
+
+export let title = 'Country'
+
+let country = ''
+
+let countryOptions: any = {}
+
+$: $countries.forEach((country: Country) => (countryOptions[country.name] = country.name))
+
+const dispatch = createEventDispatcher()
+
+const isCountryValid = (countryName: string) => $countries.some((country) => country.name === countryName)
+
+const updateCountry = (event: CustomEvent) => {
+  if (isCountryValid(event.detail)) {
+    country = event.detail
+    dispatch('chosen', country)
+  } else {
+    setNotice('Please select a country from the list')
+  }
+}
+</script>
+
+<span class="header">{title}<span class="required">*</span></span>
+<SearchableSelect choice={country} options={countryOptions} placeholder="Enter country" on:check={updateCountry} />

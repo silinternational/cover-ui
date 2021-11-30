c914f25906634818813cffa6f9e40e7b92e9a91c
hobbitronics <miguel.dw@gmail.com>
2021-11-30 11:47:36 -0700

    selectedPolicyId not reliable w/o policyId in url

--- src/pages/household.svelte
+++ src/pages/household.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
-import { selectedPolicyId } from 'data/role-policy-selection'
 import { settingsPolicy } from 'helpers/routes'
-import user from '../authn/user'
+import user, { getDefaultPolicyId } from '../authn/user'
 import { redirect } from '@roxi/routify'
 
-$: $user.policies?.length > 0 && $redirect(settingsPolicy($selectedPolicyId))
+$: $user.policies?.length > 0 && $redirect(settingsPolicy(getDefaultPolicyId($user)))
 </script>

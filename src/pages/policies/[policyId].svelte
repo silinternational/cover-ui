a9ed8c0d46627ade9779f7ed2749e3c452ff0eb1
Russell Champoux <RussellChamp@gmail.com>
2021-12-13 12:27:03 -0500

    remove unncessary lodash reduce

--- src/pages/policies/[policyId].svelte
+++ src/pages/policies/[policyId].svelte
@@ -9,7 +9,6 @@ import { formatFriendlyDate } from 'helpers/date'
 import { formatMoney } from 'helpers/money'
 import { itemDetails, settingsPolicy } from 'helpers/routes'
 import { formatPageTitle } from 'helpers/pageTitle'
-import { reduce } from 'lodash-es'
 import { metatags } from '@roxi/routify'
 import { Datatable, Page } from '@silintl/ui-components'
 import { onMount } from 'svelte'
@@ -35,8 +34,8 @@ $: claims = policy?.claims || []
 $: openClaimCount = claims.filter(claimIsOpen).length
 $: policyName = getNameOfPolicy(policy)
 $: policyName && (metatags.title = formatPageTitle(`Policies > ${policyName}`))
-$: coverage = formatMoney(reduce(activeItems, (sum, item) => sum + item.coverage_amount, 0))
-$: premium = formatMoney(reduce(activeItems, (sum, item) => sum + item.annual_premium, 0))
+$: coverage = formatMoney(activeItems.reduce((sum, item) => sum + item.coverage_amount, 0))
+$: premium = formatMoney(activeItems.reduce((sum, item) => sum + item.annual_premium, 0))
 </script>
 
 <style>

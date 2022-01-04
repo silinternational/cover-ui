998e492160b6ba48b5b2347ab58aa9a6079be0d2
Schparky <3172830+Schparky@users.noreply.github.com>
2022-01-04 08:28:25 -0700

    style and comments suggested by @RussellChamp

--- src/data/storage.ts
+++ src/data/storage.ts
@@ -1,8 +1,10 @@
 
-export function onClear(fn: clearFn): void {
+// onClear pushes a function that will be executed one and only one time, when clearApp is called
+export function onClear(fn: ClearFn): void {
   customClearFns.push(fn)
 }
 
+// clearApp executes any and all functions pushed using onClear since the last time clearApp was called
 export function clearApp(): void {
   while (customClearFns.length) {
     const fn = customClearFns.pop()
@@ -12,8 +14,8 @@ export function clearApp(): void {
   }
 }
 
-const customClearFns: Array<clearFn> = []
+const customClearFns: ClearFn[] = []
 
-interface clearFn {
+interface ClearFn {
   (): void;
 }

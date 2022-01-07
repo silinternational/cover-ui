cae0bbd8dc18c04e9555ccb5ba746698b31e3f2b
Michael Wilson <miguel.dw@gmail.com>
2022-01-07 11:47:12 -0600

    remove cards from items view and combine items

--- src/pages/policies/[policyId]/items.svelte
+++ src/pages/policies/[policyId]/items.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
-import { CardsGrid, ItemsTable, Row } from 'components'
+import { ItemsTable, Row } from 'components'
 import { isLoadingPolicyItems, loading } from 'components/progress'
-import {deleteItem, itemIsInactive, itemIsActive, loadItems, PolicyItem, selectedPolicyItems} from 'data/items'
-import { getNameOfPolicy, selectedPolicy } from 'data/policies'
+import {deleteItem, loadItems, selectedPolicyItems} from 'data/items'
 import { selectedPolicyId } from 'data/role-policy-selection'
 import * as routes from 'helpers/routes'
 import { formatPageTitle } from 'helpers/pageTitle'
@@ -11,8 +10,6 @@ import { Button, Page } from '@silintl/ui-components'
 import { onMount } from 'svelte'
 
 $: policyId = $selectedPolicyId
-$: activeItems = $selectedPolicyItems.filter(itemIsActive)
-$: inactiveItems = $selectedPolicyItems.filter(itemIsInactive)
 
 onMount(() => {
   loadItems(policyId)
@@ -29,39 +26,15 @@ const onDelete = async (event: CustomEvent<any>) => {
   loadItems(policyId)
 }
 
-const onGotoPolicyItem = (event: CustomEvent<PolicyItem>) =>
-  $goto(routes.itemDetails(event.detail.policy_id, event.detail.id))
-
 const onGotoItem = (event: CustomEvent<string>) => $goto(event.detail)
 </script>
 
 <Page layout="grid">
   <Row cols={'12'}>
-    <h3>{getNameOfPolicy($selectedPolicy)} Policy</h3>
-    <CardsGrid policyItems={$selectedPolicyItems} on:goto-item={onGotoPolicyItem} />
-  </Row>
-
-  <Row cols={'12'}>
-    {#if $selectedPolicyItems.length > 0}
-      {#if activeItems.length > 0}
-        <ItemsTable items={activeItems} {policyId} title="Active Items" on:delete={onDelete} on:gotoItem={onGotoItem} />
-      {:else}
-        <p>No active items</p>
-      {/if}
-      <br />
-      {#if inactiveItems.length > 0}
-        <ItemsTable
-          items={inactiveItems}
-          {policyId}
-          title="Inactive Items"
-          on:delete={onDelete}
-          on:gotoItem={onGotoItem}
-        />
-      {:else}
-        <p>No Inactive items</p>
-      {/if}
-    {:else if $loading && isLoadingPolicyItems(policyId)}
+    {#if $loading && isLoadingPolicyItems(policyId)}
       Loading items...
+    {:else if $selectedPolicyItems.length > 0}
+      <ItemsTable items={$selectedPolicyItems} {policyId} title="Items" on:delete={onDelete} on:gotoItem={onGotoItem} />
     {:else}
       <p class="m-0-auto text-align-center">You don't have any items in this policy</p>
       <p class="m-0-auto text-align-center">

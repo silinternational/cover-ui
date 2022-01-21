a202a9d37d4fc9e059895a39738839a3a5da5cc5
Michael Wilson <michael_wilson@sil.org>
2022-01-21 12:39:44 -0500

    use theme mdc-theme--neutral

--- src/components/ConvertCurrencyLink.svelte
+++ src/components/ConvertCurrencyLink.svelte
@@ -1,2 +1,4 @@
 To convert to USD, use
-<a href="https://www.google.com/search?q=currency+converter" target="_blank">this converter</a>.
+<a class="mdc-theme--neutral" href="https://www.google.com/search?q=currency+converter" target="_blank"
+  >this converter</a
+>.
--- src/components/Description.svelte
+++ src/components/Description.svelte
@@ -1,8 +1,1 @@
-<style>
-.description {
-  color: rgba(0, 0, 0, 0.5);
-  display: block;
-}
-</style>
-
-<span class="description"><slot /></span>
+<span class="d-block mdc-theme--neutral"><slot /></span>

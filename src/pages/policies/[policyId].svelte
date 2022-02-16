188b894e8f351650952fd222a9c1e2b5e23c6367
Michael Wilson <michael_wilson@sil.org>
2022-02-16 15:30:25 +0930

    create customer report on policy detail view

--- src/pages/policies/[policyId].svelte
+++ src/pages/policies/[policyId].svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
-import { isAdmin } from 'data/user'
+import CreateReportModal from '../admin/_components/CreateReportModal.svelte'
 import { CardsGrid, ClaimsTable, ItemsTable, Row } from 'components'
+import { month, year } from 'components/const'
 import { isLoadingById, loading } from 'components/progress'
-import { Claim, claimIsOpen, loadClaimsByPolicyId, selectedPolicyClaims } from 'data/claims'
+import { Claim, claimIsOpen, ClaimStatus, loadClaimsByPolicyId, selectedPolicyClaims } from 'data/claims'
 import { deleteItem, itemIsApproved, itemIsActive, loadItems, selectedPolicyItems, PolicyItem } from 'data/items'
 import { getNameOfPolicy, loadPolicy, Policy, PolicyType, selectedPolicy } from 'data/policies'
+import { LedgerReportType } from 'data/ledger'
 import { roleSelection } from 'data/role-policy-selection'
+import { isAdmin } from 'data/user'
 import { formatFriendlyDate } from 'helpers/dates'
 import { formatMoney } from 'helpers/money'
 import { formatPageTitle } from 'helpers/pageTitle'
@@ -19,6 +22,9 @@ export let policyId: string
 let policy = {} as Policy
 let showAllItems = false
 let showAllClaims = false
+let modalOpen = false
+let reportDate: Date
+let reportType: LedgerReportType
 
 onMount(async () => {
   policy = await loadPolicy(policyId)
@@ -32,7 +38,7 @@ $: members = policy.members || []
 $: policyId && loadItems(policyId)
 // sort items so inactive is last
 $: items = $selectedPolicyItems.filter(itemIsActive)
-$: itemsForTable = showAllItems? $selectedPolicyItems : items.slice(0, 15)
+$: itemsForTable = showAllItems ? $selectedPolicyItems : items.slice(0, 15)
 $: allItemsBtnDisabled = itemsForTable.length >= $selectedPolicyItems.length
 $: approvedItems = items.filter(itemIsApproved)
 
@@ -45,6 +51,7 @@ $: policyName = getNameOfPolicy(policy)
 $: policyName && (metatags.title = formatPageTitle(`Policies > ${policyName}`))
 $: coverage = formatMoney(approvedItems.reduce((sum, item) => sum + item.coverage_amount, 0))
 $: premium = formatMoney(approvedItems.reduce((sum, item) => sum + item.annual_premium, 0))
+$: entityCode = policy.entity_code?.code
 
 const isRecent = (claim: Claim) => {
   const incidentDate = new Date(claim.incident_date)
@@ -53,12 +60,9 @@ const isRecent = (claim: Claim) => {
   return Number(incidentDate) >= Number(aMonthAgo)
 }
 
-const onGotoClaim = (event: CustomEvent<Claim>) =>
-  $goto(customerClaimDetails(event.detail.policy_id, event.detail.id))
+const onGotoClaim = (event: CustomEvent<Claim>) => $goto(customerClaimDetails(event.detail.policy_id, event.detail.id))
 
-const onGotoPolicyItem = (event: CustomEvent<PolicyItem>) =>
-  $goto(itemDetails(event.detail.policy_id, event.detail.id))
-
+const onGotoPolicyItem = (event: CustomEvent<PolicyItem>) => $goto(itemDetails(event.detail.policy_id, event.detail.id))
 
 const onDelete = async (event: CustomEvent<any>) => {
   const itemId = event.detail
@@ -67,6 +71,42 @@ const onDelete = async (event: CustomEvent<any>) => {
   // Depending on if the item was a draft or approved it will either be deleted or updated
   // Just reload the list for now since the delete endpoint doesn't yet tell us what happened
   loadItems(policyId)
+}
+
+const claimIsApproved = (claim: Claim) => claim.status === ClaimStatus.Approved
+
+const claimIsWithinTimeframe = (claim: Claim) => {
+  const incidentDate = new Date(claim.incident_date)
+  const timeframe = reportType === LedgerReportType.monthly ? month : year
+  return Number(reportDate) - Number(incidentDate) <= timeframe
+}
+
+function createReport(e: CustomEvent) {
+  reportType = e.detail.type
+  reportDate = new Date(e.detail.date)
+  const claimPayouts = $selectedPolicyClaims
+    .filter(claimIsApproved)
+    .filter(claimIsWithinTimeframe)
+    .map((claim) => [claim.reference_number, claim.total_payout / 100])
+  const premiums = $selectedPolicyItems
+    .filter(itemIsApproved)
+    .map((item) => [item.name, (item.prorated_annual_premium || item.annual_premium) / -100]) //this is limited to current premiums as the UI doens't see payment dates
+  const transactions = [...claimPayouts, ...premiums]
+  const total = Number(transactions.reduce((sum, [, amount]) => sum + amount, 0)).toFixed(2)
+  const csvHeader = `data:text/csv;charset=utf-8,Cover Customer ${reportType} Report,${e.detail.date},\n`
+  const accountHeader =
+    policy.type === PolicyType.Team
+      ? `Policy Name,Account Number,Cost Center,Entity Code,\n${policyName},${policy.account},${policy.cost_center},${entityCode}`
+      : `Policy Name,Household ID\n${policyName},${policy.household_id}`
+  const csvContent: string =
+    csvHeader +
+    accountHeader +
+    `,\nClaim or Item, Credit/Debit,\n` +
+    transactions.map((e: any) => e.join(',')).join('\n') +
+    `,\nTotal,${total}`
+  const encodedUri = encodeURI(csvContent)
+  window.open(encodedUri)
+  modalOpen = false
 }
 </script>
 
@@ -90,9 +130,6 @@ th {
   font-weight: normal;
   font-size: small;
   padding-left: 0.5rem;
-}
-.bottom-padding {
-  padding: 2rem;
 }
 .item-footer {
   margin: 1rem auto 0 auto;
@@ -138,7 +175,7 @@ th {
         </tr>
         <tr>
           <th>Entity Code</th>
-          <td>{policy.entity_code?.code || '-'}</td>
+          <td>{entityCode || '-'}</td>
         </tr>
       {:else if policy.type === PolicyType.Household}
         <tr>
@@ -184,7 +221,7 @@ th {
 
   <div class="flex justify-between align-items-center">
     <h4>Claims <span class="subtext">({openClaimCount} open)</span></h4>
-    <Button disabled={allClaimsBtnDisabled} on:click={() => showAllClaims = true}>All Claims…</Button>
+    <Button disabled={allClaimsBtnDisabled} on:click={() => (showAllClaims = true)}>All Claims…</Button>
   </div>
   {#if $loading && isLoadingById(`policies/${policyId}/claims`)}
     Loading claims...
@@ -194,16 +231,20 @@ th {
 
   <div class="flex justify-between align-items-center">
     <h4>Items <span class="subtext">({approvedItems?.length} covered)</span></h4>
-    <Button disabled={allItemsBtnDisabled} on:click={() => showAllItems = true}>All Items…</Button>
+    <Button disabled={allItemsBtnDisabled} on:click={() => (showAllItems = true)}>All Items…</Button>
   </div>
   {#if $loading && isLoadingById(`policies/${policyId}/items`)}
     Loading items...
   {:else}
-    <ItemsTable items={itemsForTable} {policyId} on:delete={onDelete} on:gotoItem={(e) => $goto(e.detail)}/>
+    <ItemsTable items={itemsForTable} {policyId} on:delete={onDelete} on:gotoItem={(e) => $goto(e.detail)} />
     <div class="text-align-center">
       <p class="item-footer">Showing {itemsForTable.length} out of {$selectedPolicyItems.length} items</p>
       <Button url={itemsRoute(policyId)}>View {$selectedPolicyItems.length - itemsForTable.length} more items…</Button>
     </div>
   {/if}
-  <div class="bottom-padding" />
+
+  <Button on:click={() => (modalOpen = true)}>download a report</Button>
+  <CreateReportModal {modalOpen} on:submit={createReport} on:cancel={() => (modalOpen = false)} />
+
+  <div class="p-2" />
 </Page>

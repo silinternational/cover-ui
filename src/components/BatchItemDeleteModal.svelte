c4d35661721d2265f7f8d49c4ac8e28e948c7a7a
Michael Wilson <michael_wilson@sil.org>
2022-02-24 17:08:30 +0930

    add BatchItemDeleteModal

---
+++ src/components/BatchItemDeleteModal.svelte
@@ -0,0 +1,31 @@
+<script type="ts">
+import { Button, Dialog } from '@silintl/ui-components'
+import { createEventDispatcher } from 'svelte'
+
+export let openModal = false
+
+const dispatch = createEventDispatcher<{ closed: string }>()
+
+const buttons: Dialog.AlertButton[] = [
+  { label: 'Yes, Delete', action: 'delete', class: 'error-button' },
+  { label: 'cancel', action: 'cancel', class: 'mdc-dialog__button' },
+]
+
+$: message = `Are you sure you would like to delete the selected items?`
+
+const handleDialog = (choice: string) => {
+  openModal = false
+  dispatch('closed', choice)
+}
+</script>
+
+<Button on:click={() => (openModal = true)}>Delete Selected Items</Button>
+
+<Dialog.Alert
+  open={openModal}
+  {buttons}
+  defaultAction="cancel"
+  title={'Delete/Remove Coverage'}
+  on:chosen={(e) => handleDialog(e.detail)}
+  on:closed={handleDialog}>{message}</Dialog.Alert
+>

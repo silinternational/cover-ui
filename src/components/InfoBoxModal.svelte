a578e3e4b8569aa22e7cedaae4daa28649462445
Michael Wilson <michael_wilson@sil.org>
2022-03-15 06:56:07 +0930

    dispatch not needed

--- src/components/InfoBoxModal.svelte
+++ src/components/InfoBoxModal.svelte
@@ -2,7 +2,6 @@
 import user from 'data/user'
 import { itemEdit, settingsPolicy, SETTINGS_PERSONAL } from 'helpers/routes'
 import { Dialog } from '@silintl/ui-components'
-import { createEventDispatcher } from 'svelte'
 
 export let open = false
 export let sidebarDetailsArray: any[] = []
@@ -10,22 +9,10 @@ export let policyId = ''
 export let i: number
 export let itemId: string
 
-const dispatch = createEventDispatcher()
 const getAssingee = (i: number, array: any[]) => array[i - 1]['Assigned To']
-const onClosed = () => {
-  open = false
-  dispatch('closed')
-}
 </script>
 
-<Dialog.Alert
-  {open}
-  buttons={[]}
-  defaultAction="cancel"
-  title="Why is this location empty?"
-  titleIcon="info"
-  on:closed={onClosed}
->
+<Dialog.Alert {open} buttons={[]} defaultAction="cancel" title="Why is this location empty?" titleIcon="info" on:closed>
   {#if getAssingee(i, sidebarDetailsArray)}
     <p>
       Locations are associated with people, not items. {getAssingee(i, sidebarDetailsArray) || 'The assigned person'} doesn’t

1381fdf3b6907c8e7435dcd709f4f3bfc1816af0
Michael Wilson <michael_wilson@sil.org>
2022-03-15 08:28:27 +0930

    getLedgerReports not for customer

--- src/components/CustomerReport.svelte
+++ src/components/CustomerReport.svelte
@@ -1,24 +1,15 @@
 <script lang="ts">
 import CreateCustomerReportModal from './CreateCustomerReportModal.svelte'
 import type { Policy } from 'data/policies'
-import { createPolicyLedgerReport, getLedgerReports, LedgerReport, LedgerReportType } from 'data/ledger'
+import { createPolicyLedgerReport, LedgerReport, LedgerReportType } from 'data/ledger'
 import { formatDateAndTime, formatFriendlyDate } from 'helpers/dates'
 import FileLink from './FileLink.svelte'
 import { Button, Datatable } from '@silintl/ui-components'
-import { onMount } from 'svelte'
 
 export let policy: Policy
 
 let ledgerReports: LedgerReport[] = []
 let modalOpen = false
-
-onMount(() => {
-  loadReports()
-})
-
-async function loadReports() {
-  ledgerReports = await getLedgerReports()
-}
 
 async function createReport(e: CustomEvent) {
   const reportType: LedgerReportType = e.detail.type
@@ -26,32 +17,30 @@ async function createReport(e: CustomEvent) {
 
   const report = await createPolicyLedgerReport(policy.id, reportType, month, year)
   ledgerReports = [...ledgerReports, report]
-  loadReports()
   modalOpen = false
 }
 </script>
 
-<div class="flex justify-between align-items-center">
-  <h4>Reports</h4>
-  <Button on:click={() => (modalOpen = true)}>create a report</Button>
-  <CreateCustomerReportModal {modalOpen} on:submit={createReport} on:cancel={() => (modalOpen = false)} />
-</div>
+<Button on:click={() => (modalOpen = true)}>create a report</Button>
+<CreateCustomerReportModal {modalOpen} on:submit={createReport} on:cancel={() => (modalOpen = false)} />
 
-<Datatable>
-  <Datatable.Header>
-    <Datatable.Header.Item>Report Type</Datatable.Header.Item>
-    <Datatable.Header.Item>Report Date</Datatable.Header.Item>
-    <Datatable.Header.Item>Created At</Datatable.Header.Item>
-    <Datatable.Header.Item>File</Datatable.Header.Item>
-  </Datatable.Header>
-  <Datatable.Data>
-    {#each ledgerReports as report (report.id)}
-      <Datatable.Data.Row>
-        <Datatable.Data.Row.Item>{report.type || ''}</Datatable.Data.Row.Item>
-        <Datatable.Data.Row.Item>{formatFriendlyDate(report.date)}</Datatable.Data.Row.Item>
-        <Datatable.Data.Row.Item>{formatDateAndTime(report.created_at)}</Datatable.Data.Row.Item>
-        <Datatable.Data.Row.Item><FileLink file={report.file} /></Datatable.Data.Row.Item>
-      </Datatable.Data.Row>
-    {/each}
-  </Datatable.Data>
-</Datatable>
+{#if ledgerReports.length > 0}
+  <Datatable>
+    <Datatable.Header>
+      <Datatable.Header.Item>Report Type</Datatable.Header.Item>
+      <Datatable.Header.Item>Report Date</Datatable.Header.Item>
+      <Datatable.Header.Item>Created At</Datatable.Header.Item>
+      <Datatable.Header.Item>File</Datatable.Header.Item>
+    </Datatable.Header>
+    <Datatable.Data>
+      {#each ledgerReports as report (report.id)}
+        <Datatable.Data.Row>
+          <Datatable.Data.Row.Item>{report.type || ''}</Datatable.Data.Row.Item>
+          <Datatable.Data.Row.Item>{formatFriendlyDate(report.date)}</Datatable.Data.Row.Item>
+          <Datatable.Data.Row.Item>{formatDateAndTime(report.created_at)}</Datatable.Data.Row.Item>
+          <Datatable.Data.Row.Item><FileLink file={report.file} /></Datatable.Data.Row.Item>
+        </Datatable.Data.Row>
+      {/each}
+    </Datatable.Data>
+  </Datatable>
+{/if}

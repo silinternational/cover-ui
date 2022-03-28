b745f88d1754f1144c16b339ef559bafa5f9100e
Michael Wilson <michael_wilson@sil.org>
2022-03-28 17:02:28 +0930

    extract callback to isAdminReport

--- src/pages/admin/reports.svelte
+++ src/pages/admin/reports.svelte
@@ -22,6 +22,8 @@ async function createReport(e: CustomEvent) {
   await createLedgerReport(e.detail.type, e.detail.date)
   await loadReports()
 }
+// TODO: remove this filter when BE filters out policy reports
+const isAdminReport = (report: LedgerReport) => !report.file?.name.includes('policy')
 </script>
 
 <Page>
@@ -40,7 +42,7 @@ async function createReport(e: CustomEvent) {
     </Datatable.Header>
     <Datatable.Data>
       <!-- TODO: remove this filter when BE filters out policy reports -->
-      {#each ledgerReports.filter((r) => !r.file?.name.includes('policy')) as report (report.id)}
+      {#each ledgerReports.filter(isAdminReport) as report (report.id)}
         <Datatable.Data.Row on:click={() => $goto(reportDetails(report.id))} clickable>
           <Datatable.Data.Row.Item>{report.type || ''}</Datatable.Data.Row.Item>
           <Datatable.Data.Row.Item>{formatFriendlyDate(report.date)}</Datatable.Data.Row.Item>

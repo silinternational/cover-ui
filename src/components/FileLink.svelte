05a3649b04648c3085f1c266f86ce388e1fa4f09
Michael Wilson <michael_wilson@sil.org>
2022-03-31 17:24:22 +0930

    add on:expired to FileLink

--- src/components/FileLink.svelte
+++ src/components/FileLink.svelte
@@ -1,14 +1,18 @@
 <script lang="ts">
 import type { CoverFile } from 'data/file'
+import { createEventDispatcher } from 'svelte'
 
 export let file: CoverFile
 
-let expired
+let expired: boolean
+
+const dispatch = createEventDispatcher<{ expired: string }>()
 
 $: url = (!expired && file?.url) || undefined
 $: filename = file?.name || '-'
-$: expiration = file?.url_expiration && new Date(file.url_expiration)
-$: expiration && setTimeout(() => (expired = true), expiration - new Date())
+$: expiration = file?.url_expiration && Number(new Date(file.url_expiration))
+$: expiration && setTimeout(() => (expired = true), expiration - Number(new Date()))
+$: expired && dispatch('expired')
 </script>
 
 <a href={url}>{filename}</a>

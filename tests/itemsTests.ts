136d20db8c554e2215a95a0ddb1d194548b39d4b
Michael Wilson <miguel.dw@gmail.com>
2022-06-09 21:37:33 +0930

    Add generic test funciton

--- tests/itemsTests.ts
+++ tests/itemsTests.ts
@@ -1,6 +1,6 @@
 import { itemsByPolicyId, loadItems } from '../src/data/items'
 import user, { loadUser } from '../src/data/user'
-import { get } from 'svelte/store'
+import { get, Writable } from 'svelte/store'
 
 const objectIsCorrectShape = (obj: any, propertyNames: string[]): boolean => {
   const objectKeys = Object.keys(obj)
@@ -21,6 +21,25 @@ export async function testLoadItems(): Promise<boolean> {
     const items = entry[1]
     return items.every((item) => objectIsCorrectShape(item, expectedProperties))
   })
+  console.log('passed: ', passed)
+  return passed
+}
+
+// test any function
+export async function testFunctionThatGetsArray(
+  functionToTest,
+  storeOfthingLoaded: Writable<[]>,
+  expectedProperties: string[],
+  ...parameters
+): Promise<boolean> {
+  console.log('expected properties: ', expectedProperties)
+  await functionToTest(parameters)
+  const policyId = get(user).policies[0].id
+  storeOfthingLoaded.set([])
+  console.log('things set to []:', get(storeOfthingLoaded))
+  await functionToTest(policyId)
+  console.log('things after calling function:', get(storeOfthingLoaded))
+  const passed = get(storeOfthingLoaded).every((e) => objectIsCorrectShape(e, expectedProperties))
   console.log('passed: ', passed)
   return passed
 }

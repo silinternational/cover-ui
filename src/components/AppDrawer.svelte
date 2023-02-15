5afb5d4565b174b679fbd8de3faeab9eee5a41ea
Michael Wilson <michael_wilson@sil.org>
2023-02-15 15:02:29 +0930

    hide the RoleAndPolicyMenu when anonymous

--- src/components/AppDrawer.svelte
+++ src/components/AppDrawer.svelte
@@ -14,6 +14,7 @@ import { onMount } from 'svelte'
 export let menuItems: any[]
 export let myPolicies: Policy[]
 export let role: UserAppRole
+export let userIsAnonymous: boolean
 
 const isNotProduction = process.env.CF_PAGES_BRANCH !== 'main'
 
@@ -57,14 +58,16 @@ const logoClickHandler = () => $goto(ROOT)
   title="Covered"
   class="drawer border-white {$showApp ? 'opacity1' : 'opacity0'}"
 >
-  <span class="pointer" on:click={logoClickHandler} slot="header">
+  <span class="pointer" on:click={logoClickHandler} on:keyup slot="header">
     <img class="logo" src="/logo.svg" alt="Cover" />
   </span>
 
   <AppHeader on:toggleDrawer={() => (toggle = !toggle)} />
 
   <div class="role-and-policy-menu pt-1" slot="drawer-content-top">
-    <RoleAndPolicyMenu {myPolicies} {role} on:policy on:role />
+    {#if !userIsAnonymous}
+      <RoleAndPolicyMenu {myPolicies} {role} on:policy on:role />
+    {/if}
   </div>
 
   <slot />

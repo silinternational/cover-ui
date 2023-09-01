c0ba9a0883dc710092e1e8584ad8722910c262b1
Matt H <matt_henderson@sil.org>
2023-09-01 15:52:05 -0400

    Move arrow outside of convert-currency link

--- src/components/ConvertCurrencyLink.svelte
+++ src/components/ConvertCurrencyLink.svelte
@@ -1,3 +1,3 @@
 <a class="mdc-theme--neutral" href="https://www.google.com/search?q=currency+converter" target="_blank" rel="noreferrer"
-  >Convert your currency to USD ↗</a
->
+  >Convert your currency to USD</a
+> ↗

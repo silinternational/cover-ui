ba6005e2bfa22859dcf25b4d9ac209cfbf1348ec
Alex Mercado <alex_mercado@sil.org>
2023-09-15 21:18:56 -0500

    This is a test for the purpose of discussion.
    * Install tailwind autoprefixer @tailwindcss/typography github:domchristie/tailwind-utopia prettier-plugin-tailwindcss
    
    Requires `npm install -D --force` or `make bun` to run.

--- rollup.config.js
+++ rollup.config.js
@@ -62,6 +62,7 @@ export default {
           includePaths: ['node_modules'],
         },
       },
+      plugins: [require('tailwindcss'), require('autoprefixer')],
     }),
     routify({
       dynamicImports: false,
@@ -72,15 +73,11 @@ export default {
     production ? terser() : livereload('dist'),
     html({
       template: ({ files }) => {
-        const script = (files.js || [])
-          .map(({ fileName }) => `<script src='/${fileName}'></script>`)
-          .join('\n')
+        const script = (files.js || []).map(({ fileName }) => `<script src='/${fileName}'></script>`).join('\n')
 
-        const css = (files.css || [])
-          .map(({ fileName }) => `<link rel='stylesheet' href='/${fileName}'>`)
-          .join('\n')
+        const css = (files.css || []).map(({ fileName }) => `<link rel='stylesheet' href='/${fileName}'>`).join('\n')
         return getHtml(script, css)
-      }
+      },
     }),
   ],
   watch: {
@@ -88,7 +85,7 @@ export default {
   },
 }
 
-function getHtml (script, css) {
+function getHtml(script, css) {
   return `<!DOCTYPE html>
   <html lang="en">
   <head>

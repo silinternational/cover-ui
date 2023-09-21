b7a1c27ed40c6cd7bb49ecde7f74e1ac5039c2f9
Matt H <matt_henderson@sil.org>
2023-09-21 13:45:14 -0400

    Remove no-longer-used `selectedCategoryIsVehicle` variable

--- src/components/forms/ItemForm.svelte
+++ src/components/forms/ItemForm.svelte
@@ -65,7 +65,6 @@ $: make,
   accountablePersonId && categoryId && name && debouncedSave()
 $: selectedCategory = $categories.find((c) => c.id === categoryId)
 $: selectedCategoryIsStationary = selectedCategory?.risk_category?.name === RiskCategoryNames.Stationary
-$: selectedCategoryIsVehicle = selectedCategory?.risk_category?.name === RiskCategoryNames.Vehicle
 $: shortNameExample = assembleShortNameExample(make, model)
 
 const debouncedSave = debounce(() => saveForLater(undefined, true), 4000)

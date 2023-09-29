6f535dd7c1b508520dfbc6e1567b2a9f4d1feb1a
Matt H <matt_henderson@sil.org>
2023-09-29 16:41:35 -0400

    Show minimum deductible during checkout, when applicable

--- src/components/ItemDetails.svelte
+++ src/components/ItemDetails.svelte
@@ -45,9 +45,12 @@ $: teamDetails = {
   'Cost Center': policy.cost_center,
   'Internal Account': policy.account,
 }
+$: minimumDeductible = item?.category?.minimum_deductible
+$: minimumDeductibleDescription = minimumDeductible ? `(subject to ${formatMoney(minimumDeductible)} minimum)` : ''
 $: moneyDetails = {
   Value: formatMoney(item.coverage_amount),
   Premium: `${formatMoney(item.annual_premium)} / yr`,
+  Deductible: `5% ${minimumDeductibleDescription}`.trim()
 }
 $: sidebarDetailsArray =
   policy.type === PolicyType.Team

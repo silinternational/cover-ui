ef5ca4f492b8ec8f69cb047ea1a901206779d858
Matt H <matt_henderson@sil.org>
2023-10-05 13:50:20 -0400

    Fix `formatDate()` to treat Go empty-date values as not a date

--- src/helpers/dates.ts
+++ src/helpers/dates.ts
@@ -8,6 +8,11 @@ export const formatFriendlyDate = (dateTimeString: string): string => {
   return formatDate(dateTimeString.split('T')[0], { month: 'numeric', day: 'numeric', year: 'numeric' })
 }
 
+/**
+ * Take a date string and format it for display to the user. If no timezone is included in the date
+ * string, UTC is assumed. Returns an empty string for date strings that probably indicate no-data
+ * (such as a date near the Unix epoch, or a date like "0001-01-01").
+ */
 export const formatDate = (
   dateString: string | undefined,
   options: Intl.DateTimeFormatOptions | undefined = { month: 'long', day: 'numeric', year: 'numeric' }
@@ -19,8 +24,13 @@ export const formatDate = (
       // if the date is within a day of the epoch assume it is the epoch since local timestring could be used
       return ''
     }
-    // When the dateString does not contain a time portion, treat it as a UTC date
+
+    if (date.toISOString() === '0001-01-01T00:00:00.000Z') {
+      return ''
+    }
+
     if (dateString.indexOf('T') === -1) {
+      // When the dateString does not contain a time portion, treat it as a UTC date
       options.timeZone = 'UTC'
     }
     return date.toLocaleDateString('default', options)

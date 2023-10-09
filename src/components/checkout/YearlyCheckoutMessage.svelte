394156de9c4d8db9ab8909391905aa3b1229257f
Michael Wilson <michael_wilson@sil.org>
2023-10-10 07:46:51 +0930

    check for no payment, don't split mid sentence

--- src/components/checkout/YearlyCheckoutMessage.svelte
+++ src/components/checkout/YearlyCheckoutMessage.svelte
@@ -8,17 +8,24 @@ export let item: PolicyItem | undefined = undefined
 export let org: string = ''
 
 $: annualPremium = item?.annual_premium
-$: proratedAnnualPremiumOrMin = Math.max(item?.prorated_annual_premium || 0, item?.category?.minimum_premium || 0)
+$: proratedAnnualPremium = item?.prorated_annual_premium || 0
 
 $: year = new Date().getFullYear()
 $: renewYear = year + 1
 $: renewDate = formatDate(`${renewYear}-01-01`)
-$: premiumEqualsProrated = annualPremium === proratedAnnualPremiumOrMin
+$: premiumEqualsProrated = annualPremium === proratedAnnualPremium
 </script>
 
 <span>
-  Pay {formatMoney(proratedAnnualPremiumOrMin)}
-  {premiumEqualsProrated ? 'for' : 'for the remainder of'}
-  {year} from {org} account
-  {accountOrHouseholdId}. Auto-renew and pay {formatMoney(annualPremium)} on {renewDate}.
+  {#if proratedAnnualPremium > 0}
+    {#if premiumEqualsProrated}
+      Pay {formatMoney(proratedAnnualPremium)} for {year} from {org} account {accountOrHouseholdId}.
+    {:else}
+      Pay {formatMoney(proratedAnnualPremium)} for the remainder of {year} from {org} account
+      {accountOrHouseholdId}.
+    {/if}
+    Auto-renew and pay {formatMoney(annualPremium)} on {renewDate}.
+  {:else}
+    No payment needed right now. Auto-renew for {formatMoney(annualPremium)} on {renewDate}, paid from {org} account {accountOrHouseholdId}.
+  {/if}
 </span>

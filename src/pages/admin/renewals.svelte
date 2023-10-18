9cfc6d56ebab47503603dd23233ac7378ddc19e2
Matt H <matt_henderson@sil.org>
2023-10-18 16:29:52 -0400

    Improve separation of "Annual" and "Monthly" sections of Renewals page

--- src/pages/admin/renewals.svelte
+++ src/pages/admin/renewals.svelte
@@ -8,7 +8,7 @@ import {
 } from 'data/ledger'
 import { formatPageTitle } from 'helpers/pageTitle'
 import { metatags } from '@roxi/routify'
-import { Button, Page, setNotice } from '@silintl/ui-components'
+import { Button, Card, Page, setNotice } from '@silintl/ui-components'
 import { onMount } from 'svelte'
 
 let isProcessingAnnual = false
@@ -50,27 +50,31 @@ metatags.title = formatPageTitle('Admin > Renewals')
 <Page>
   <h1>Renewals</h1>
 
-  <h2>Annual</h2>
-  <p>Annual process for renewing coverage</p>
-  <p>
-    Number of items to renew: {annualStatus?.items_to_process?.toLocaleString() || '…'}
-    {#if isProcessingAnnual && !annualStatus.is_complete}
-      <Button class="ml-1" on:click={onClickRefreshAnnual}>Refresh</Button>
-    {/if}
-  </p>
-  <div class="my-1">
-    <Button raised on:click={onClickProcessAnnual} disabled={disableProcessAnnualButton}>Process Annual</Button>
-  </div>
+  <Card class="tw-w-96 tw-max-w-full tw-my-6">
+    <h2>Annual</h2>
+    <p>Annual process for renewing coverage</p>
+    <p>
+      Number of items to renew: {annualStatus?.items_to_process?.toLocaleString() || '…'}
+      {#if isProcessingAnnual && !annualStatus.is_complete}
+        <Button class="ml-1" on:click={onClickRefreshAnnual}>Refresh</Button>
+      {/if}
+    </p>
+    <div class="my-1">
+      <Button raised on:click={onClickProcessAnnual} disabled={disableProcessAnnualButton}>Process Annual</Button>
+    </div>
+  </Card>
 
-  <h2>Monthly</h2>
-  <p>Monthly process for renewing coverage: This should be done between the 20th and the end of the month.</p>
-  <p>
-    Number of items to renew: {monthlyStatus?.items_to_process?.toLocaleString() || '…'}
-    {#if isProcessingMonthly && !monthlyStatus.is_complete}
-      <Button class="ml-1" on:click={onClickRefreshMonthly}>Refresh</Button>
-    {/if}
-  </p>
-  <div class="my-1">
-    <Button raised on:click={onClickProcessMonthly} disabled={disableProcessMonthlyButton}>Process Monthly</Button>
-  </div>
+  <Card class="tw-w-96 tw-max-w-full tw-my-6">
+    <h2>Monthly</h2>
+    <p>Monthly process for renewing coverage: This should be done between the 20th and the end of the month.</p>
+    <p>
+      Number of items to renew: {monthlyStatus?.items_to_process?.toLocaleString() || '…'}
+      {#if isProcessingMonthly && !monthlyStatus.is_complete}
+        <Button class="ml-1" on:click={onClickRefreshMonthly}>Refresh</Button>
+      {/if}
+    </p>
+    <div class="my-1">
+      <Button raised on:click={onClickProcessMonthly} disabled={disableProcessMonthlyButton}>Process Monthly</Button>
+    </div>
+  </Card>
 </Page>

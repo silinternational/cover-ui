5e982733693312654323734b85a45b797f0c2035
Matt H <matt_henderson@sil.org>
2023-10-18 16:28:45 -0400

    Add `h1` title, hint when to run monthly, improve button text (renewals)

--- src/pages/admin/renewals.svelte
+++ src/pages/admin/renewals.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-  import {
-    getAnnualPolicyRenewalStatus,
-    getMonthlyPolicyRenewalStatus,
-    processAnnualPolicyRenewals,
-    processMonthlyPolicyRenewals,
-    RenewalStatus
-  } from 'data/ledger'
+import {
+  getAnnualPolicyRenewalStatus,
+  getMonthlyPolicyRenewalStatus,
+  processAnnualPolicyRenewals,
+  processMonthlyPolicyRenewals,
+  RenewalStatus
+} from 'data/ledger'
 import { formatPageTitle } from 'helpers/pageTitle'
 import { metatags } from '@roxi/routify'
 import { Button, Page, setNotice } from '@silintl/ui-components'
@@ -48,27 +48,29 @@ metatags.title = formatPageTitle('Admin > Renewals')
 </script>
 
 <Page>
-  <h2>Annual Renewals</h2>
+  <h1>Renewals</h1>
+
+  <h2>Annual</h2>
   <p>Annual process for renewing coverage</p>
   <p>
     Number of items to renew: {annualStatus?.items_to_process?.toLocaleString() || '…'}
     {#if isProcessingAnnual && !annualStatus.is_complete}
-      <Button class="ml-1" on:click={onClickRefreshAnnual}>refresh</Button>
+      <Button class="ml-1" on:click={onClickRefreshAnnual}>Refresh</Button>
     {/if}
   </p>
   <div class="my-1">
-    <Button raised on:click={onClickProcessAnnual} disabled={disableProcessAnnualButton}>process</Button>
+    <Button raised on:click={onClickProcessAnnual} disabled={disableProcessAnnualButton}>Process Annual</Button>
   </div>
 
-  <h2>Monthly Renewals</h2>
-  <p>Monthly process for renewing coverage</p>
+  <h2>Monthly</h2>
+  <p>Monthly process for renewing coverage: This should be done between the 20th and the end of the month.</p>
   <p>
     Number of items to renew: {monthlyStatus?.items_to_process?.toLocaleString() || '…'}
     {#if isProcessingMonthly && !monthlyStatus.is_complete}
-      <Button class="ml-1" on:click={onClickRefreshMonthly}>refresh</Button>
+      <Button class="ml-1" on:click={onClickRefreshMonthly}>Refresh</Button>
     {/if}
   </p>
   <div class="my-1">
-    <Button raised on:click={onClickProcessMonthly} disabled={disableProcessMonthlyButton}>process</Button>
+    <Button raised on:click={onClickProcessMonthly} disabled={disableProcessMonthlyButton}>Process Monthly</Button>
   </div>
 </Page>
